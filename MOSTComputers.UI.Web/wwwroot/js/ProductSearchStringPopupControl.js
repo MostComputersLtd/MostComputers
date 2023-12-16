995b3f88ee6a2c5f20bdaf204f7f05ddfb99c6e5
Daniel Nikolov <danielsnikolov@gmail.com>
2023-12-16 16:18:55 +0200

    Fixed the transportToSearchStringOriginDisplay to move scrollbar of origins display list to the correct location. Added a method to get index of item from its id and a method to toggle and highlight the multiple possible origins list.

--- MOSTComputers.UI.Web/wwwroot/js/ProductSearchStringPopupControl.js
+++ MOSTComputers.UI.Web/wwwroot/js/ProductSearchStringPopupControl.js
@@ -40,8 +40,7 @@ function colorSearchStringOriginDisplayAndGiveItABorder(searchStringPartValue, c
 
     if (borderWidth !== null && borderColor !== null)
     {
-        searchStringPartOriginDisplayToSelect.style.borderColor = borderColor;
-        searchStringPartOriginDisplayToSelect.style.borderWidth = borderWidth;
+        searchStringPartOriginDisplayToSelect.style.border = borderWidth + " solid " + borderColor;
         searchStringPartOriginDisplayToSelect.style.borderRadius = borderRadius;
     }
 }
@@ -50,9 +49,14 @@ function transportToSearchStringOriginDisplay(searchStringPartValue)
 {
     var searchStringPartOriginDisplayToSelect = findSearchStringPartOriginsDisplayFromSearchStringPart(searchStringPartValue);
 
+    var searchStringTopDisplayList = document.getElementById("ProductSearchString_popup_searchStringPartDisplay_ul");
+    var modalHeader = document.getElementById("ProductSearchString_modal-header");
+    var modalFooter = document.getElementById("ProductSearchString_modal-footer");
+
     var searchStringPartOriginDisplayList = document.getElementById("ProductSearchString_popup_searchStringPartsOrigin_ul");
 
-    searchStringPartOriginDisplayList.scrollTop = (searchStringPartOriginDisplayToSelect.offsetTop - searchStringPartOriginDisplayToSelect.clientHeight + searchStringPartOriginDisplayToSelect.style.marginTop);
+    searchStringPartOriginDisplayList.scrollTop =
+        (searchStringPartOriginDisplayToSelect.offsetTop - searchStringTopDisplayList.offsetHeight - modalHeader.offsetHeight - modalFooter.offsetHeight + 28)
 }
 
 function findSearchStringPartOriginsDisplayFromSearchStringPart(searchStringPartValue)
@@ -79,4 +83,56 @@ function findSearchStringPartOriginsDisplayFromSearchStringPart(searchStringPart
     }
 
     return null;
+}
+
+function getIndexOfItemFromId(id)
+{
+    if (id === null
+        || id === undefined) return null;
+
+    var searchStringPartLowerDisplayIndex = id.indexOf("#") + 1;
+
+    var indexAsString = id.substring(searchStringPartLowerDisplayIndex);
+
+    return indexAsString;
+}
+
+function ProductSearchStringPopup_searchStringPartOrigin_multipleOriginsShow(index, substringToHighlight)
+{
+    if (index === null
+        || index === undefined) return;
+
+    var listOfOptionsToShow = document.getElementById("ProductSearchStringPopup_searchStringPartOrigin_multipleOriginsDisplayList#" + index);
+
+    var nameLabels = document.getElementsByName("ProductSearchStringPopup_searchStringPartOrigin_multipleOriginsDisplayList_nameLabel");
+    var meaningLabels = document.getElementsByName("ProductSearchStringPopup_searchStringPartOrigin_multipleOriginsDisplayList_meaningLabel");
+
+    var allLabels = [];
+
+    console.log(nameLabels);
+    console.log(meaningLabels);
+
+    for (var i = 0; i < nameLabels.length; i++)
+    {
+        allLabels.push(nameLabels[i]);
+    }
+
+    for (var i = 0; i < meaningLabels.length; i++)
+    {
+        allLabels.push(meaningLabels[i]);
+    }
+
+    if (listOfOptionsToShow.style.visibility === "collapse")
+    {
+        listOfOptionsToShow.style.visibility = "visible";
+
+
+        highlightAllElementsTextsWhereValueIsPresent(allLabels, substringToHighlight, "#FAF2B1", "customSearchStringSelectSpan");
+    }
+    else
+    {
+        listOfOptionsToShow.style.visibility = "collapse";
+
+        removeHighlightAllElementsTextsWhereValueIsPresent(allLabels, substringToHighlight)
+    }
 }
