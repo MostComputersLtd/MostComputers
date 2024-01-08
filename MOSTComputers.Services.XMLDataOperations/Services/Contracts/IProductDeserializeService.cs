2ad8afd6e484e6735a69e8bfae15d9ace0492271
Daniel Nikolov <danielsnikolov@gmail.com>
2024-01-08 15:48:19 +0200

    Added interfaces for the services that lacked them.

---
+++ MOSTComputers.Services.XMLDataOperations/Services/Contracts/IProductDeserializeService.cs
@@ -0,0 +1,11 @@
+﻿using MOSTComputers.Services.XMLDataOperations.Models;
+using OneOf;
+
+namespace MOSTComputers.Services.XMLDataOperations.Services.Contracts;
+public interface IProductDeserializeService
+{
+    XmlObjectData? DeserializeProductsXml(string xml);
+    string SerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes);
+    OneOf<XmlObjectData?, InvalidXmlResult> TryDeserializeProductsXml(string xml);
+    OneOf<string?, InvalidXmlResult> TrySerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes);
+}
