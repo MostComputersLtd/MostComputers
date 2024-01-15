55ace623bb7a6eab22f9f2d93eeea984dff7eed3
Daniel Nikolov <danielsnikolov@gmail.com>
2024-01-15 15:59:03 +0200

    Added logic to add, drag and drop and delete images

--- MOSTComputers.UI.Web/wwwroot/js/Pages/Shared/ProductImagesPopupControl.js
+++ MOSTComputers.UI.Web/wwwroot/js/Pages/Shared/ProductImagesPopupControl.js
@@ -66,3 +66,141 @@ function getProductId()
 
     return parseInt(productId);
 }
+
+var indexOfDraggedItem;
+
+function productImageDisplay_ul_li_ondragstart(e)
+{
+    indexOfDraggedItem = getImageDisplayOrder(e.target);
+
+    setTimeout(() =>
+    {
+        e.target.classList.add("draggedImageLi");
+    }, 0);
+
+}
+
+function productImageDisplay_ul_ondragover(e)
+{
+    const productImageDisplayList = document.getElementById("productImageDisplay_ul");
+
+    const childrenOfListThatArentDragged = [...productImageDisplayList.querySelectorAll("li:not(.draggedImageLi)")];
+
+    const itemThatIsDragged = productImageDisplayList.querySelector(".draggedImageLi");
+
+    var nextSibling = childrenOfListThatArentDragged.find(child =>
+    {
+        return e.clientX <= child.getBoundingClientRect().left + (child.clientWidth / 2);
+    });
+
+    if (nextSibling != undefined)
+    {
+        productImageDisplayList.insertBefore(itemThatIsDragged, nextSibling);
+    }
+}
+
+function productImageDisplay_ul_li_ondragend(e)
+{
+    setTimeout(() =>
+    {
+        e.target.classList.remove("draggedImageLi");
+    }, 0);
+
+    const productId = getProductId();
+
+    var newIndexOfDraggedItem = getImageDisplayOrder(e.target);
+
+    if (indexOfDraggedItem === newIndexOfDraggedItem) return;
+
+    const url = "/ProductDisplay/" + "?handler=UpdateImageOrder" + "&productId=" + productId + "&oldDisplayOrder=" + indexOfDraggedItem + "&newDisplayOrder=" + newIndexOfDraggedItem;
+
+    $.ajax({
+        type: "PUT",
+        url: url,
+        contentType: "application/json",
+        data: null,
+        headers: {
+            RequestVerificationToken:
+                $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+    })
+        .done(function (result) {
+
+            //var imagesPopupModalContent = document.getElementById("ProductImages_popup_modal-content");
+
+            //imagesPopupModalContent.innerHTML = result;
+        })
+        .fail(function (jqXHR, textStatus) {
+        });
+}
+
+function getImageDisplayOrder(target)
+{
+    const productImageDisplayList = document.getElementById("productImageDisplay_ul");
+
+    const childrenArray = Array.from(productImageDisplayList.children);
+
+    var newIndexOfDraggedItem = childrenArray.indexOf(target);
+
+    return newIndexOfDraggedItem + 1;
+}
+
+function addNewImage(fileInputElement)
+{
+    var productId = getProductId();
+
+    if (productId === null) return;
+
+    var formBody = new FormData();
+
+    formBody.append("fileInfo", fileInputElement.files[0])
+
+    var url = "/ProductDisplay/" + "?handler=AddNewImageToProduct" + "&productId=" + productId;
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: false,
+        processData: false,
+        data: formBody,
+        headers: {
+            RequestVerificationToken:
+                $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+    })
+        .done(function (result) {
+
+            var imagesPopupModalContent = document.getElementById("ProductImages_popup_modal-content");
+
+            imagesPopupModalContent.innerHTML = result;
+        })
+        .fail(function (jqXHR, textStatus) {
+        });
+}
+
+function delete_productImageDisplay_ul_li(elementId, imageId)
+{
+    const url = "/ProductDisplay/" + "?handler=DeleteImageFromProduct" + "&imageId=" + imageId;
+
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        contentType: "application/json",
+        data: null,
+        headers: {
+            RequestVerificationToken:
+                $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+    })
+        .done(function (result)
+        {
+            const productImageDisplayList = document.getElementById("productImageDisplay_ul");
+
+            const productImageListItemToDelete = document.getElementById(elementId);
+
+            productImageDisplayList.removeChild(productImageListItemToDelete)
+        })
+        .fail(function (jqXHR, textStatus)
+        {
+        });
+}
