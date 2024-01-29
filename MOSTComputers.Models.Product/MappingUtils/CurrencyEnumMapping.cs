81f7ab2ce3a48d70582b2b5e37a70977b72f7442
Daniel Nikolov <danielsnikolov@gmail.com>
2024-01-29 16:56:32 +0200

    Moved common mapping logic to model enums in the MOSTComputers.Models.Product project.

---
+++ MOSTComputers.Models.Product/MappingUtils/CurrencyEnumMapping.cs
@@ -0,0 +1,32 @@
+﻿using MOSTComputers.Models.Product.Models;
+
+namespace MOSTComputers.Models.Product.MappingUtils;
+
+public static class CurrencyEnumMapping
+{
+    public static string? GetStringFromCurrencyEnum(CurrencyEnum? currencyEnum)
+    {
+        if (currencyEnum is null) return null;
+
+        return currencyEnum switch
+        {
+            CurrencyEnum.BGN => "BGN",
+            CurrencyEnum.EUR => "EUR",
+            CurrencyEnum.USD => "USD",
+            _ => null
+        };
+    }
+
+    public static CurrencyEnum? GetCurrencyEnumFromString(string currencyString)
+    {
+        if (currencyString is null) return null;
+
+        return currencyString switch
+        {
+            "BGN" => CurrencyEnum.BGN,
+            "EUR" => CurrencyEnum.EUR,
+            "USD" => CurrencyEnum.USD,
+            _ => null
+        };
+    }
+}
---
+++ MOSTComputers.Models.Product/MappingUtils/ProductStatusMapping.cs
@@ -0,0 +1,52 @@
+﻿using MOSTComputers.Models.Product.Models;
+
+namespace MOSTComputers.Models.Product.MappingUtils;
+
+public static class ProductStatusMapping
+{
+    public static string? GetStatusStringFromStatusEnum(ProductStatusEnum productStatusEnum)
+    {
+        return productStatusEnum switch
+        {
+            ProductStatusEnum.Unavailable => "Unavailable",
+            ProductStatusEnum.Available => "Available",
+            ProductStatusEnum.Call => "Call",
+            _ => null
+        };
+    }
+
+    public static ProductStatusEnum? GetStatusEnumFromStatusString(string statusString)
+    {
+        return statusString switch
+        {
+            "Unavailable" => ProductStatusEnum.Unavailable,
+            "Available" => ProductStatusEnum.Available,
+            "Call" => ProductStatusEnum.Call,
+            _ => null
+        };
+    }
+
+    public static string? GetBGStatusStringFromStatusEnum(ProductStatusEnum? productStatusEnum)
+    {
+        if (productStatusEnum is null) return null;
+
+        return productStatusEnum switch
+        {
+            ProductStatusEnum.Unavailable => "Не е в наличност",
+            ProductStatusEnum.Available => "В наличност",
+            ProductStatusEnum.Call => "Обадете се",
+            _ => null
+        };
+    }
+
+    public static ProductStatusEnum? GetStatusEnumFromBGStatusString(string statusString)
+    {
+        return statusString switch
+        {
+            "Не е в наличност" => ProductStatusEnum.Unavailable,
+            "В наличност" => ProductStatusEnum.Available,
+            "Обадете се" => ProductStatusEnum.Call,
+            _ => null
+        };
+    }
+}
