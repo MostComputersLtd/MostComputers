4a087e410638f8da2b7e8fb09f93f7d53262d147
Daniel Nikolov <danielsnikolov@gmail.com>
2024-01-29 17:12:01 +0200

    Added a model to represent a grouping for pref purposes

---
+++ MOSTComputers.Services.ProductRegister/Models/Grouping/Grouping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.ProductRegister.Models.Grouping;
+
+internal class Grouping<TKey, TElement> : List<TElement>, IGrouping<TKey, TElement>
+{
+    internal Grouping(TKey key) : base() => Key = key;
+    internal Grouping(TKey key, int capacity) : base(capacity) => Key = key;
+    internal Grouping(TKey key, IEnumerable<TElement> collection)
+        : base(collection) => Key = key;
+    public TKey Key { get; }
+}
