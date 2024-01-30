5c8331f58b8985cb2dbc26e5ea4e904ef28c9c33
Daniel Nikolov <danielsnikolov@gmail.com>
2024-01-30 08:22:26 +0200

    Init

---
+++ MOSTComputers.Services.XMLDataOperations/Services/Contracts/IProductToXmlProductMappingService.cs
@@ -0,0 +1,8 @@
+﻿using MOSTComputers.Models.Product.Models;
+using MOSTComputers.Services.XMLDataOperations.Models;
+
+namespace MOSTComputers.Services.XMLDataOperations.Services.Contracts;
+public interface IProductToXmlProductMappingService
+{
+    XmlProduct MapToXmlProduct(Product product);
+}
