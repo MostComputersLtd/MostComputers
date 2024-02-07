dcc4306b32c6137a23ff7fec612da1e540e21a92
Daniel Nikolov <danielsnikolov@gmail.com>
2024-02-07 08:47:13 +0200

    Added a validator for LocalChangeData

---
+++ MOSTComputers.Services.LocalChangesHandling/Validation/LocalChangeDataValidatorForGivenTableAndOperationType.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MOSTComputers.Models.Product.Models.Changes;
+using MOSTComputers.Models.Product.Models.Changes.Local;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.LocalChangesHandling.Validation;
+
+internal sealed class LocalChangeDataValidatorForGivenTableAndOperationType : AbstractValidator<LocalChangeData>
+{
+    public LocalChangeDataValidatorForGivenTableAndOperationType(string tableName, ChangeOperationTypeEnum changeOperationType)
+    {
+        RuleFor(x => x.Id).GreaterThan(0);
+        RuleFor(x => x.TableName).Must(tableName => !string.IsNullOrWhiteSpace(tableName))
+            .WithMessage("Argument must not be empty or whitespace")
+            .Equal(tableName);
+
+        RuleFor(x => x.OperationType).Equal(changeOperationType);
+        RuleFor(x => x.TableElementId).GreaterThan(0);
+        RuleFor(x => x.TimeStamp).GreaterThan(new DateTime(0));
+    }
+}
