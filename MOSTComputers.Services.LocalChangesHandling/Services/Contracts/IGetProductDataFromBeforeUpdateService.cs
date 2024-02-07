429b3b5790dd1eb19296b1538514866ff1fe5352
Daniel Nikolov <danielsnikolov@gmail.com>
2024-02-07 08:43:12 +0200

    Added a service to abstract the process of getting the data of a product before update.

---
+++ MOSTComputers.Services.LocalChangesHandling/Services/Contracts/IGetProductDataFromBeforeUpdateService.cs
@@ -0,0 +1,8 @@
+﻿using MOSTComputers.Models.Product.Models;
+
+namespace MOSTComputers.Services.LocalChangesHandling.Services.Contracts;
+public interface IGetProductDataFromBeforeUpdateService
+{
+    Product? GetProductBeforeUpdate(uint productId);
+    bool HandleAfterUpdate(uint productId);
+}
---
+++ MOSTComputers.Services.LocalChangesHandling/Services/GetProductDataFromBeforeUpdateService.cs
@@ -0,0 +1,27 @@
+﻿using MOSTComputers.Models.Product.Models;
+using MOSTComputers.Services.LocalChangesHandling.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.LocalChangesHandling.Services;
+
+internal sealed class GetProductDataFromBeforeUpdateService : IGetProductDataFromBeforeUpdateService
+{
+    public GetProductDataFromBeforeUpdateService()
+    {
+
+    }
+
+    public Product? GetProductBeforeUpdate(uint productId)
+    {
+        return null;
+    }
+
+    public bool HandleAfterUpdate(uint productId)
+    {
+        return true;
+    }
+}
