a5e571019c14c4bc91da44ed1c3c30eb81c5da9d
Daniel Nikolov <danielsnikolov@gmail.com>
2024-03-27 09:45:32 +0200

    Changed how image mapping is done by using both ProductImage and ProductImageFileNameInfo data.

--- MOSTComputers.Services.XMLDataOperations/Services/Mapping/ProductToXmlProductMappingService.cs
+++ MOSTComputers.Services.XMLDataOperations/Services/Mapping/ProductToXmlProductMappingService.cs
@@ -22,7 +22,7 @@ internal sealed class ProductToXmlProductMappingService : IProductToXmlProductMa
             Code = product.Name ?? string.Empty,
             CurrencyCode = GetStringFromCurrencyEnum(product.Currency) ?? string.Empty,
             SearchString = product.SearchString ?? string.Empty,
-            ShopItemImages = product.ImageFileNames is not null ? GetXmlImageDataFromImageFilePathInfos(product.ImageFileNames, _initialImageFileNameInfoPath) : new(),
+            ShopItemImages = GetXmlImageDataFromImageFilePathInfos(product.Images, product.ImageFileNames, _initialImageFileNameInfoPath),
             PartNumbers = GetPartNumberXmlStringFromPartNumbers(product.PartNumber1, product.PartNumber2),
             Category = product.Category is not null ? Map(product.Category) : new(),
             Manifacturer = product.Manifacturer is not null ? Map(product.Manifacturer) : new(),
@@ -63,20 +63,99 @@ internal sealed class ProductToXmlProductMappingService : IProductToXmlProductMa
         };
     }
 
-    private static List<XmlShopItemImage> GetXmlImageDataFromImageFilePathInfos(List<ProductImageFileNameInfo> productImageFileNameInfos, string initialPath)
+    private static List<XmlShopItemImage> GetXmlImageDataFromImageFilePathInfos(
+        List<ProductImage>? productImages,
+        List<ProductImageFileNameInfo>? productImageFileNameInfos,
+        string initialPath)
     {
         List<XmlShopItemImage> output = new();
 
-        foreach (ProductImageFileNameInfo fileNameInfo in productImageFileNameInfos)
+        if (productImages is null)
         {
-            XmlShopItemImage xmlShopItem = new()
-            {
-                PictureUrl = initialPath + fileNameInfo.FileName,
-                ThumbnailUrl = initialPath + fileNameInfo.FileName,
-                DisplayOrder = (short)(fileNameInfo.DisplayOrder ?? 0)
-            };
+            if (productImageFileNameInfos is null) return new();
 
-            output.Add(xmlShopItem);
+            foreach (ProductImageFileNameInfo fileNameInfo in productImageFileNameInfos)
+            {
+                XmlShopItemImage xmlShopItem = new()
+                {
+                    PictureUrl = initialPath + fileNameInfo.FileName,
+                    ThumbnailUrl = initialPath + fileNameInfo.FileName,
+                    DisplayOrder = (short)(fileNameInfo.DisplayOrder ?? 0)
+                };
+
+                output.Add(xmlShopItem);
+            }
+
+            return output;
+        }
+
+        if (productImageFileNameInfos is null)
+        {
+            for (int i = 0; i < productImages.Count; i++)
+            {
+                ProductImage productImage = productImages[i];
+
+                string? fileName = null;
+
+                if (productImage.ImageFileExtension is not null)
+                {
+                    int indexOfSlashInFileType = productImage.ImageFileExtension.IndexOf('/');
+
+                    if (indexOfSlashInFileType == -1)
+                    {
+                        fileName = $"{productImage.Id}";
+                    }
+
+                    string fileExtensionFromImageFileType = productImage.ImageFileExtension[(indexOfSlashInFileType + 1)..];
+
+                    fileName = $"{productImage.Id}.{fileExtensionFromImageFileType}";
+                }
+
+                XmlShopItemImage xmlShopItem = new()
+                {
+                    PictureUrl = initialPath + fileName ?? "",
+                    ThumbnailUrl = initialPath + fileName ?? "",
+                    DisplayOrder = (short)(i)
+                };
+
+                output.Add(xmlShopItem);
+            }
+
+            return output;
+        }
+
+        for (int i = 0; i < productImages.Count; i++)
+        {
+            ProductImage productImage = productImages[i];
+
+            if (productImage.ImageFileExtension is not null)
+            {
+                int indexOfSlashInFileType = productImage.ImageFileExtension.IndexOf('/');
+
+                string fileExtensionFromImageFileType = productImage.ImageFileExtension[(indexOfSlashInFileType + 1)..];
+
+                string fileName = $"{productImage.Id}.{fileExtensionFromImageFileType}";
+
+                ProductImageFileNameInfo? relatedFileNameInfo = productImageFileNameInfos.Find(imageFileName =>
+                    imageFileName.FileName == fileName);
+
+                XmlShopItemImage xmlShopItem = new()
+                {
+                    PictureUrl = initialPath + fileName ?? "",
+                    ThumbnailUrl = initialPath + fileName ?? "",
+                    DisplayOrder = (short)(relatedFileNameInfo?.DisplayOrder ?? i)
+                };
+
+                output.Add(xmlShopItem);
+
+                output = output.OrderBy(x => x.DisplayOrder)
+                    .ToList();
+
+                for (int k = 0; k < output.Count; k++)
+                {
+                    output[k].DisplayOrder = (short)(k + 1);
+                }
+            }
         }
 
         return output;
