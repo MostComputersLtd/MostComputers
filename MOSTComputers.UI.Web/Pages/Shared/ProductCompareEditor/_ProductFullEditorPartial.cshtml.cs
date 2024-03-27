fec2470fb62cce6e704097349313f1f5367439bf
Daniel Nikolov <danielsnikolov@gmail.com>
2024-03-27 11:24:17 +0200

    Added an XmlProduct property to display the VendorUrl of the product.

--- MOSTComputers.UI.Web/Pages/Shared/ProductCompareEditor/_ProductFullEditorPartial.cshtml.cs
+++ MOSTComputers.UI.Web/Pages/Shared/ProductCompareEditor/_ProductFullEditorPartial.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MOSTComputers.Models.Product.Models;
 using MOSTComputers.Services.SearchStringOrigin.Models;
+using MOSTComputers.Services.XMLDataOperations.Models;
 using static MOSTComputers.UI.Web.Pages.ProductCompareEditorModel;
 
 namespace MOSTComputers.UI.Web.Pages.Shared.ProductCompareEditor;
@@ -9,6 +10,7 @@ namespace MOSTComputers.UI.Web.Pages.Shared.ProductCompareEditor;
 public class ProductFullEditorPartialModel
 {
     public required Product? Product { get; set; }
+    public required XmlProduct? XmlProduct { get; set; }
     public required List<Tuple<string, List<SearchStringPartOriginData>?>>? ProductSearchStringPartsAndDataAboutTheirOrigin { get; set; }
     public required List<ProductCharacteristic>? CharacteristicsRelatedToProduct { get; set; }
     public required string ElementIdAndNamePrefix { get; init; }
