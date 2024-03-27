1fd7da4a4a9dff003f6e2a065097727947ee66bd
Daniel Nikolov <danielsnikolov@gmail.com>
2024-03-27 09:43:43 +0200

    added a disableXsiAndXsdNamespaces bool to all SerializeProductsXml methods give consumers the option to remove xsi: and xsd: namespaces from generated xml.

--- MOSTComputers.Services.XMLDataOperations/Services/Contracts/IProductDeserializeService.cs
+++ MOSTComputers.Services.XMLDataOperations/Services/Contracts/IProductDeserializeService.cs
@@ -7,8 +7,8 @@ public interface IProductDeserializeService
 {
     XmlObjectData? DeserializeProductsXml(string xml);
     OneOf<XmlObjectData?, InvalidXmlResult> TryDeserializeProductsXml(string xml);
-    string SerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes);
-    string SerializeProductsXml(Product product, bool shouldRemoveXmlSerializeEscapes);
-    OneOf<string?, InvalidXmlResult> TrySerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes);
-    OneOf<string?, InvalidXmlResult> TrySerializeProductsXml(Product product, bool shouldRemoveXmlSerializeEscapes);
+    string SerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes, bool disableXsiAndXsdNamespaces = false);
+    string SerializeProductsXml(Product product, bool shouldRemoveXmlSerializeEscapes, bool disableXsiAndXsdNamespaces = false);
+    OneOf<string?, InvalidXmlResult> TrySerializeProductsXml(XmlObjectData xmlObjectData, bool shouldRemoveXmlSerializeEscapes, bool disableXsiAndXsdNamespaces = false);
+    OneOf<string?, InvalidXmlResult> TrySerializeProductsXml(Product product, bool shouldRemoveXmlSerializeEscapes, bool disableXsiAndXsdNamespaces = false);
 }
