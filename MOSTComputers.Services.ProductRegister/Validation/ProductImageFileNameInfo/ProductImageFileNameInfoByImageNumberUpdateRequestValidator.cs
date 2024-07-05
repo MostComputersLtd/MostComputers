32ec75f8d1a2aa5d70f95e48cd7beb35d52d3b07
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 14:16:28 +0300

    Renamed ProductImageFileNameInfoUpdateRequestValidator to ProductImageFileNameInfoByImageNumberUpdateRequestValidator, Added ImageNumber property to ProductImageFileNameInfo.

--- MOSTComputers.Services.ProductRegister/Validation/ProductImageFileNameInfo/ProductImageFileNameInfoByImageNumberUpdateRequestValidator.cs
+++ MOSTComputers.Services.ProductRegister/Validation/ProductImageFileNameInfo/ProductImageFileNameInfoByImageNumberUpdateRequestValidator.cs
@@ -1,22 +1,17 @@
 ﻿using FluentValidation;
-using MOSTComputers.Models.Product.Models.Requests.ProductImageFileNameInfo;
 using MOSTComputers.Services.ProductRegister.Models.Requests.ProductImageFileNameInfo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static MOSTComputers.Services.ProductRegister.Validation.CommonElements;
 
 namespace MOSTComputers.Services.ProductRegister.Validation.ProductImageFileNameInfo;
 
-internal sealed class ProductImageFileNameInfoUpdateRequestValidator : AbstractValidator<ServiceProductImageFileNameInfoUpdateRequest>
+internal sealed class ProductImageFileNameInfoByImageNumberUpdateRequestValidator
+    : AbstractValidator<ServiceProductImageFileNameInfoByImageNumberUpdateRequest>
 {
-    public ProductImageFileNameInfoUpdateRequestValidator()
+    public ProductImageFileNameInfoByImageNumberUpdateRequestValidator()
     {
         RuleFor(x => x.ProductId).GreaterThan(0);
         RuleFor(x => x.FileName).Must(IsNotEmptyOrWhiteSpace).MaximumLength(50);
-        RuleFor(x => x.DisplayOrder).Must(NullOrGreaterThanZero);
         RuleFor(x => x.NewDisplayOrder).Must(NullOrGreaterThanZero);
+        RuleFor(x => x.ImageNumber).GreaterThan(0);
     }
 }
