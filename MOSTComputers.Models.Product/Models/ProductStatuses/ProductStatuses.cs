453edad0d6d762a8ff30e96417b37d03c2efb0d2
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 13:31:46 +0300

    Moved ProductStatuses from MOSTComputers.Models.Product.Models to MOSTComputers.Models.Product.Models.ProductStatuses

--- MOSTComputers.Models.Product/Models/ProductStatuses/ProductStatuses.cs
+++ MOSTComputers.Models.Product/Models/ProductStatuses/ProductStatuses.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MOSTComputers.Models.Product.Models;
+namespace MOSTComputers.Models.Product.Models.ProductStatuses;
 
 public sealed class ProductStatuses
 {
