94f90ff03759cdcbc36e018a4a17d174b6276199
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 13:19:00 +0300

    Renamed ProductImage.XML to HtmlData in all relevant requests.

--- MOSTComputers.Models.Product/Models/Requests/ProductImage/ProductFirstImageCreateRequest.cs
+++ MOSTComputers.Models.Product/Models/Requests/ProductImage/ProductFirstImageCreateRequest.cs
@@ -3,7 +3,7 @@
 public sealed class ProductFirstImageCreateRequest
 {
     public int? ProductId { get; set; }
-    public string? XML { get; set; }
+    public string? HtmlData { get; set; }
     public byte[]? ImageData { get; set; }
     public string? ImageFileExtension { get; set; }
     public DateTime? DateModified { get; set; }
