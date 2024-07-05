6195442fd9f2bc01da44898e088f096a2f78d855
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 14:02:21 +0300

    Renamed ServiceProductImageFileNameInfoUpdateRequest to ServiceProductImageFileNameInfoByImageNumberUpdateRequest

--- MOSTComputers.Services.ProductRegister/Models/Requests/ProductImageFileNameInfo/ServiceProductImageFileNameInfoByImageNumberUpdateRequest.cs
+++ MOSTComputers.Services.ProductRegister/Models/Requests/ProductImageFileNameInfo/ServiceProductImageFileNameInfoByImageNumberUpdateRequest.cs
@@ -1,10 +1,10 @@
 ﻿namespace MOSTComputers.Services.ProductRegister.Models.Requests.ProductImageFileNameInfo;
 
-public sealed class ServiceProductImageFileNameInfoUpdateRequest
+public sealed class ServiceProductImageFileNameInfoByImageNumberUpdateRequest
 {
     public int ProductId { get; set; }
     public string? FileName { get; set; }
-    public int? DisplayOrder { get; set; }
     public int? NewDisplayOrder { get; set; }
+    public int ImageNumber { get; set; }
     public bool? Active { get; set; }
 }
