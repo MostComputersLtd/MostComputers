172b69a8dd25968f45834fa02836bbaf894debc4
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 13:39:13 +0300

    Added to map between the new custom MemoryCacheEntryOptions abstraction and the .NET Microsoft.Extensions.Caching.Memory.MemoryCacheEntryOptions

---
+++ MOSTComputers.Services.Caching/Mapping/LazyCacheMappingUtils.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using MOSTComputers.Services.Caching.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.Caching.Mapping;
+internal static class LazyCacheMappingUtils
+{
+    internal static MemoryCacheEntryOptions Map(CustomMemoryCacheEntryOptions customOptions)
+    {
+        MemoryCacheEntryOptions output = new()
+        {
+            AbsoluteExpiration = customOptions.AbsoluteExpiration,
+            AbsoluteExpirationRelativeToNow = customOptions.AbsoluteExpirationRelativeToNow,
+            SlidingExpiration = customOptions.SlidingExpiration,
+            Priority = Map(customOptions.Priority),
+            Size = customOptions.Size,
+        };
+
+        foreach (IChangeToken changeToken in customOptions.ExpirationTokens)
+        {
+            output.AddExpirationToken(changeToken);
+        }
+
+        return output;
+    }
+
+    internal static CacheItemPriority Map(CustomCacheItemPriorityEnum priority)
+    {
+        return priority switch
+        {
+            CustomCacheItemPriorityEnum.Low => CacheItemPriority.Low,
+            CustomCacheItemPriorityEnum.Normal => CacheItemPriority.Normal,
+            CustomCacheItemPriorityEnum.High => CacheItemPriority.High,
+            CustomCacheItemPriorityEnum.NeverRemove => CacheItemPriority.NeverRemove,
+            _ => throw new NotImplementedException()
+        };
+    }
+}
