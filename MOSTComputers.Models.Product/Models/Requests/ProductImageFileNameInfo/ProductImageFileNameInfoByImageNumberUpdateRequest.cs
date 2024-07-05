3be9dddaad0d5fedec484fa4a374b8981f0d6ae1
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 13:21:31 +0300

    Renamed ProductImageFileNameInfoUpdateRequest to ProductImageFileNameInfoByImageNumberUpdateRequest and changed it to use the new ImageNumber property instead of the DisplayOrder for identifying the record to update

--- MOSTComputers.Models.Product/Models/Requests/ProductImageFileNameInfo/ProductImageFileNameInfoByImageNumberUpdateRequest.cs
+++ MOSTComputers.Models.Product/Models/Requests/ProductImageFileNameInfo/ProductImageFileNameInfoByImageNumberUpdateRequest.cs
@@ -1,10 +1,10 @@
 ﻿namespace MOSTComputers.Models.Product.Models.Requests.ProductImageFileNameInfo;
 
-public sealed class ProductImageFileNameInfoUpdateRequest
+public sealed class ProductImageFileNameInfoByImageNumberUpdateRequest
 {
     public int ProductId { get; set; }
     public string? FileName { get; set; }
-    public int? DisplayOrder { get; set; }
     public int? NewDisplayOrder { get; set; }
+    public int ImageNumber { get; set; }
     public bool Active { get; set; }
 }
