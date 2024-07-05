b71d727f97063a8dcf3513978d302bff6562f79b
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 13:34:37 +0300

    Added ProductWorkStatuses

---
+++ MOSTComputers.Services.DAL/Mapping/ProductWorkStatusesEntityMap.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.FluentMap.Mapping;
+using MOSTComputers.Models.Product.Models.ProductStatuses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.DAL.Mapping;
+internal sealed class ProductWorkStatusesEntityMap : EntityMap<ProductWorkStatuses>
+{
+    public ProductWorkStatusesEntityMap()
+    {
+        Map(x => x.Id).ToColumn("ProductWorkStatusId");
+        Map(x => x.ProductId).ToColumn("ProductWorkStatusProductId");
+        Map(x => x.ProductNewStatus).ToColumn("ProductNewStatus");
+        Map(x => x.ProductXmlStatus).ToColumn("ProductXmlReadyStatus");
+        Map(x => x.ReadyForImageInsert).ToColumn("ReadyForImageInsertStatus");
+    }
+}
