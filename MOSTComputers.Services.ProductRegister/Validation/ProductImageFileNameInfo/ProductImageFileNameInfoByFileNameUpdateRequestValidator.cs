0f87a289de1e1485b039547edd06a8472adc19e9
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 14:16:51 +0300

    Added

---
+++ MOSTComputers.Services.ProductRegister/Validation/ProductImageFileNameInfo/ProductImageFileNameInfoByFileNameUpdateRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MOSTComputers.Services.ProductRegister.Models.Requests.ProductImageFileNameInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static MOSTComputers.Services.ProductRegister.Validation.CommonElements;
+
+namespace MOSTComputers.Services.ProductRegister.Validation.ProductImageFileNameInfo;
+internal class ProductImageFileNameInfoByFileNameUpdateRequestValidator
+    : AbstractValidator<ServiceProductImageFileNameInfoByFileNameUpdateRequest>
+{
+    public ProductImageFileNameInfoByFileNameUpdateRequestValidator()
+    {
+        RuleFor(x => x.ProductId).GreaterThan(0);
+        RuleFor(x => x.FileName).Must(IsNotEmptyOrWhiteSpace).MaximumLength(50);
+        RuleFor(x => x.NewDisplayOrder).Must(NullOrGreaterThanZero);
+        RuleFor(x => x.NewFileName).Must(IsNotEmptyOrWhiteSpace).MaximumLength(50);
+    }
+}
