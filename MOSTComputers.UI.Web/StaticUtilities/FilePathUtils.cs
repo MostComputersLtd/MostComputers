456149656e1c57c46f3d21cf264dc33d088b8fb1
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 16:07:00 +0300

    Init

---
+++ MOSTComputers.UI.Web/StaticUtilities/FilePathUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace MOSTComputers.UI.Web.StaticUtilities;
+
+internal static class FilePathUtils
+{
+    internal static string? AddSlashAtTheStart(string? url)
+    {
+        if (string.IsNullOrWhiteSpace(url)
+            || url[0] == '/')
+        {
+            return url;
+        }
+
+        return "/" + url;
+    }
+
+    internal static string? RemoveSlashAtTheStart(string? url)
+    {
+        if (string.IsNullOrWhiteSpace(url)
+            || url[0] != '/')
+        {
+            return url;
+        }
+
+        return url[1..];
+    }
+}
