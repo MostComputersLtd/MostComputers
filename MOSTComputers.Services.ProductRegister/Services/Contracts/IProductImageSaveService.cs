29e2373ed7b60eb5ae179365d6d3600940663267
Daniel Nikolov <danielsnikolov@gmail.com>
2024-07-05 14:05:15 +0300

    Added ProductImageFileSaveService

---
+++ MOSTComputers.Services.ProductRegister/Services/Contracts/IProductImageSaveService.cs
@@ -0,0 +1,9 @@
+﻿using MOSTComputers.Models.Product.Models;
+
+namespace MOSTComputers.Services.ProductRegister.Services.Contracts;
+public interface IProductImageSaveService
+{
+    bool AddImageToProduct(int productId, ProductImage image, ProductImageFileNameInfo productImageFileNameInfo);
+    bool AddOrUpdateImagesOfProduct(int productId, Dictionary<ProductImage, ProductImageFileNameInfo> productUpdatedImages);
+    Dictionary<ProductImage, ProductImageFileNameInfo>? GetImagesForProduct(int productId);
+}
