934ab39e7e053134bff651e5aa887b448e9913ad
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 21:28:14 +0300

    Added logic to display different data based on the ProductImagePopupUsageEnum enum.

--- MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/ProductPopups/_ProductImagesDisplayPopupPartial.cshtml.cs
+++ MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/ProductPopups/_ProductImagesDisplayPopupPartial.cshtml.cs
@@ -1,19 +1,99 @@
+using MOSTComputers.Models.Product.Models;
+using MOSTComputers.Services.ProductImageFileManagement.Models;
+using MOSTComputers.Services.ProductImageFileManagement.Services;
+using MOSTComputers.Services.ProductRegister.Services.Contracts;
 using MOSTComputers.UI.Web.RealWorkTesting.Models.Product;
-using static MOSTComputers.UI.Web.RealWorkTesting.Utils.MappingUtils.ProductToDisplayDataMappingUtils;
+using MOSTComputers.Utils.ProductImageFileNameUtils;
+using OneOf;
+using static MOSTComputers.Utils.ProductImageFileNameUtils.ProductImageAndFileNameRelationsUtils;
+using static MOSTComputers.Utils.ProductImageFileNameUtils.ProductImageFileNameUtils;
 
 namespace MOSTComputers.UI.Web.RealWorkTesting.Pages.Shared.ProductPopups;
 
 public class ProductImagesDisplayPopupPartialModel
 {
-    public ProductImagesDisplayPopupPartialModel(ProductDisplayData productDisplayData)
+    public ProductImagesDisplayPopupPartialModel(
+        ProductImagePopupUsageEnum productImagePopupUsageEnum,
+        ProductDisplayData productDisplayData,
+        IProductImageService productImageService,
+        IProductImageFileNameInfoService productImageFileNameInfoService,
+        IProductImageFileManagementService productImageFileManagementService,
+        string? notificationBoxId = null)
     {
+        ProductImagePopupUsageEnum = productImagePopupUsageEnum;
         ProductData = productDisplayData;
+        NotificationBoxId = notificationBoxId;
 
-        if (productDisplayData.ImagesAndImageFileInfos is null
-            || productDisplayData.ImagesAndImageFileInfos.Count <= 0) return;
+        ImagesToDisplay = GetImagesToDisplayFromData(
+            productImagePopupUsageEnum, productDisplayData, productImageService, productImageFileNameInfoService, productImageFileManagementService);
 
-        productDisplayData.ImagesAndImageFileInfos = OrderImagesAndImageFileNameInfos(productDisplayData.ImagesAndImageFileInfos);
+        if (productDisplayData?.ImagesAndImageFileInfos is not null)
+        {
+            productDisplayData.ImagesAndImageFileInfos = OrderImagesAndImageFileNameInfos(productDisplayData.ImagesAndImageFileInfos);
+        }
     }
 
+    public List<ImageAndImageFileNameRelation> ImagesToDisplay { get; set; }
+    public ProductImagePopupUsageEnum ProductImagePopupUsageEnum { get; }
     public ProductDisplayData ProductData { get; set; }
+    public string? NotificationBoxId { get; }
+
+    private static List<ImageAndImageFileNameRelation> GetImagesToDisplayFromData(
+        ProductImagePopupUsageEnum productImagePopupUsageEnum,
+        ProductDisplayData productDisplayData,
+        IProductImageService productImageService,
+        IProductImageFileNameInfoService productImageFileNameInfoService,
+        IProductImageFileManagementService productImageFileManagementService)
+    {
+        if (productImagePopupUsageEnum == ProductImagePopupUsageEnum.ImagesInDatabase)
+        {
+            IEnumerable<ProductImage> productImages = productImageService.GetAllInProduct(productDisplayData.Id);
+            IEnumerable<ProductImageFileNameInfo> productImageFileNameInfos = productImageFileNameInfoService.GetAllInProduct(productDisplayData.Id);
+
+            return GetImageDictionaryFromImagesAndImageFileInfos(productImages.ToList(), productImageFileNameInfos.ToList());
+        }
+        else if (productImagePopupUsageEnum == ProductImagePopupUsageEnum.ImagesInFiles)
+        {
+            IEnumerable<ProductImageFileNameInfo> productImageFileNameInfos = productImageFileNameInfoService.GetAllInProduct(productDisplayData.Id);
+
+            List<ImageAndImageFileNameRelation> output = new();
+
+            foreach (ProductImageFileNameInfo imageFileNameInfo in productImageFileNameInfos)
+            {
+                if (imageFileNameInfo.FileName == null) continue;
+
+                OneOf<byte[], FileDoesntExistResult, NotSupportedFileTypeResult> imageFileReadResult
+                    = productImageFileManagementService.GetImage(imageFileNameInfo.FileName);
+
+                ImageAndImageFileNameRelation imageAndFileNameRelation = imageFileReadResult.Match<ImageAndImageFileNameRelation>(
+                    imageData =>
+                    {
+                        string fileExtension = Path.GetExtension(imageFileNameInfo.FileName);
+
+                        ProductImage productImageFromData = new()
+                        {
+                            ProductId = productDisplayData.Id,
+                            ImageData = imageData,
+                            ImageContentType = GetImageContentTypeFromFileExtension(fileExtension),
+                        };
+
+                        return new(productImageFromData, imageFileNameInfo);
+                    },
+                    fileDoesntExistResult => new(null, imageFileNameInfo),
+                    notSupportedFileTypeResult => new(null, imageFileNameInfo));
+
+                output.Add(imageAndFileNameRelation);
+            }
+
+            return OrderImagesAndImageFileNameInfos(output);
+        }
+
+        if (productDisplayData.ImagesAndImageFileInfos is null
+            || productDisplayData.ImagesAndImageFileInfos.Count <= 0)
+        {
+            return new();
+        }
+
+        return OrderImagesAndImageFileNameInfos(productDisplayData.ImagesAndImageFileInfos);
+    }
 }
