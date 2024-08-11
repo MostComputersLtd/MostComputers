0534ea53d58a36cf9d7c401928bcc164101f71b4
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 18:54:24 +0300

    Added so that _ProductImagesDisplayPopupPartial can be used to display various types of data

---
+++ MOSTPrices.UI.Web.RealWorkTesting/Models/Product/ProductImagePopupUsageEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace MOSTComputers.UI.Web.RealWorkTesting.Models.Product;
+
+public enum ProductImagePopupUsageEnum
+{
+    ImagesInDatabase = 0,
+    ImagesInFiles = 1,
+    DisplayData = 2,
+}
