0217c7f7dc90a66396ded653b8f3521babd7b5bf
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 19:27:23 +0300

    Renamed Image.ImageFileExtension to ImageContentType

--- MOSTComputers.Models.Product/Models/ProductImage.cs
+++ MOSTComputers.Models.Product/Models/ProductImage.cs
@@ -6,6 +6,6 @@ public sealed class ProductImage
     public int? ProductId { get; set; }
     public string? HtmlData { get; set; }
     public byte[]? ImageData { get; set; }
-    public string? ImageFileExtension { get; set; }
+    public string? ImageContentType { get; set; }
     public DateTime? DateModified { get; set; }
 }
