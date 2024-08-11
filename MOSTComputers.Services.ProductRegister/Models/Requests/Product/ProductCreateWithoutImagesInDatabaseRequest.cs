cbd90938150d2d33a496edebb0308984e70ac492
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 18:50:48 +0300

    Added for the IProductManipulateService.InsertProductWithImagesOnlyInDirectoryAsync method

---
+++ MOSTComputers.Services.ProductRegister/Models/Requests/Product/ProductCreateWithoutImagesInDatabaseRequest.cs
@@ -0,0 +1,43 @@
+﻿using MOSTComputers.Models.Product.Models;
+using MOSTComputers.Utils.ProductImageFileNameUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.ProductRegister.Models.Requests.Product;
+
+public sealed class ProductCreateWithoutImagesInDatabaseRequest
+{
+    public string? Name { get; set; }
+    public decimal? AdditionalWarrantyPrice { get; set; }
+    public long? AdditionalWarrantyTermMonths { get; set; }
+    public string? StandardWarrantyPrice { get; set; }
+    public long? StandardWarrantyTermMonths { get; set; }
+    public int? DisplayOrder { get; set; }
+    public ProductStatusEnum? Status { get; set; }
+    public int? PlShow { get; set; }
+    public decimal? Price1 { get; set; }
+    public decimal? DisplayPrice { get; set; }
+    public decimal? Price3 { get; set; }
+    public CurrencyEnum? Currency { get; set; }
+    public Guid? RowGuid { get; set; }
+    public int? PromotionId { get; set; }
+    public int? PromRid { get; set; }
+    public short? PromotionPictureId { get; set; }
+    public DateTime? PromotionExpireDate { get; set; }
+    public short? AlertPictureId { get; set; }
+    public DateTime? AlertExpireDate { get; set; }
+    public string? PriceListDescription { get; set; }
+    public string? PartNumber1 { get; set; }
+    public string? PartNumber2 { get; set; }
+    public string? SearchString { get; set; }
+    public int? CategoryId { get; set; }
+    public MOSTComputers.Models.Product.Models.Category? Category { get; set; }
+    public short? ManifacturerId { get; set; }
+    public Manifacturer? Manifacturer { get; set; }
+    public int? SubCategoryId { get; set; }
+    public List<ProductProperty>? Properties { get; set; }
+    public List<ImageAndImageFileNameRelation>? ImagesAndFileNames { get; set; }
+}
