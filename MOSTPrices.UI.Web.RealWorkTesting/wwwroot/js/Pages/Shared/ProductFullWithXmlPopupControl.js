984bfb13e6cb4525b4ad464c03ee8ac1d487dcb7
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 18:56:58 +0300

    Init

---
+++ MOSTPrices.UI.Web.RealWorkTesting/wwwroot/js/Pages/Shared/ProductFullWithXmlPopupControl.js
@@ -0,0 +1,13 @@
+﻿function open_ProductFullWithXml_modal()
+{
+    let dialog = document.getElementById("ProductFullWithXml_modal_dialog");
+
+    dialog.style.height = window.innerHeight;
+
+    $("#ProductFullWithXml_modal").modal("show");
+}
+
+function close_ProductFullWithXml_modal()
+{
+    $("#ProductFullWithXml_modal").modal("toggle");
+}
