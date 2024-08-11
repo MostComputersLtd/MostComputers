d07586cb20418d5ca49a333955affcbdfeac17cc
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 21:56:34 +0300

    Added or updated methods for populating, adding and removing product data.

--- MOSTPrices.UI.Web.RealWorkTesting/Services/Contracts/IProductTableDataService.cs
+++ MOSTPrices.UI.Web.RealWorkTesting/Services/Contracts/IProductTableDataService.cs
@@ -6,13 +6,21 @@ public interface IProductTableDataService
 {
     IReadOnlyList<ProductDisplayData> ProductData { get; }
 
-    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(IEnumerable<ProductDisplayData> products);
-    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(Func<IEnumerable<ProductDisplayData>> productFunc);
-    Task<IReadOnlyList<ProductDisplayData>> PopulateIfEmptyAndGetAsync(Func<Task<IEnumerable<ProductDisplayData>>> productFuncAsync);
-    void AddProductData(ProductDisplayData product, bool makeNewProductFirst = false);
-    void Populate(IEnumerable<ProductDisplayData> products);
+    ProductDisplayData? GetProductById(int productId);
+    IReadOnlyList<ProductDisplayData> GetProductDataToDisplay();
+    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(IEnumerable<ProductDisplayData> products, bool areNewProductsDisplayable = true);
+    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(IEnumerable<ProductDisplayData> products, Func<ProductDisplayData, bool> areNewProductsDisplayableFunc);
+    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(Func<IEnumerable<ProductDisplayData>> productFunc, bool areNewProductsDisplayable = true);
+    IReadOnlyList<ProductDisplayData> PopulateIfEmptyAndGet(Func<IEnumerable<ProductDisplayData>> productFunc, Func<ProductDisplayData, bool> areNewProductsDisplayableFunc);
+    Task<IReadOnlyList<ProductDisplayData>> PopulateIfEmptyAndGetAsync(Func<Task<IEnumerable<ProductDisplayData>>> productFuncAsync, bool areNewProductsDisplayable = true);
+    Task<IReadOnlyList<ProductDisplayData>> PopulateIfEmptyAndGetAsync(Func<Task<IEnumerable<ProductDisplayData>>> productFuncAsync, Func<ProductDisplayData, bool> areNewProductsDisplayableFunc);
+    void Populate(IEnumerable<ProductDisplayData> products, bool areNewProductsDisplayable = true);
+    void Populate(IEnumerable<ProductDisplayData> products, Func<ProductDisplayData, bool> areNewProductsDisplayableFunc);
+    bool AddProductDataOrMakeExistingOneDisplayable(ProductDisplayData product, bool makeNewProductFirst = false, bool isNewOrOldProductDisplayable = true);
+    bool AddToDisplayableProductIdsIfIdIsntAlreadyAdded(int productId);
+    void AddOrUpdateProductData(ProductDisplayData product, bool makeProductFirst = false, bool isNewProductDisplayable = true);
     bool RemoveProductByIndex(int index);
     bool RemoveProductById(int id);
-    ProductDisplayData? GetProductById(int productId);
-    bool AddProductDataIfProductDataWithSameIdDoesntExist(ProductDisplayData product, bool makeNewProductFirst = false);
+    void RemoveProductIdFromIdsToDisplay(int productId);
+    void RemoveAllIdsFromIdsToDisplay();
 }
