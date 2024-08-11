f088c39e0fba9a91bf027c7da773eaf73a36e5f5
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 22:12:17 +0300

    Removed showSearchStringOriginDataSmallPopup and removeSearchStringOriginDataSmallPopup methods.

--- MOSTPrices.UI.Web.RealWorkTesting/wwwroot/js/Pages/Shared/SearchStringOriginSmallPopupsDisplayControl.js
+++ MOSTPrices.UI.Web.RealWorkTesting/wwwroot/js/Pages/Shared/SearchStringOriginSmallPopupsDisplayControl.js
@@ -1,39 +1,3 @@
-function showSearchStringOriginDataSmallPopup(productId, index, searchStringPart, elementIdAndNamePrefix)
-{
-    if (productId === null
-        || productId === undefined
-        || (isNaN(productId) && isNaN(parseInt(productId)))
-        || index === null
-        || index === undefined
-        || (isNaN(index) && isNaN(parseInt(index)))) return;
-
-    showSearchStringOriginDataSmallPopupCommon(
-        elementIdAndNamePrefix + "searchStringPartOrigin_li-" + productId + "-" + index,
-        elementIdAndNamePrefix + "searchStringPartOrigin_multipleOriginsDisplayList-" + productId + "-" + index,
-        searchStringPart,
-        elementIdAndNamePrefix + "searchStringPartOrigin_multipleOriginsDisplayList_nameLabel-" + productId + "-" + index,
-        elementIdAndNamePrefix + "searchStringPartOrigin_multipleOriginsDisplayList_meaningLabel-" + productId + "-" + index);
-}
-
-function removeSearchStringOriginDataSmallPopup(productId, index, searchStringPart, elementIdAndNamePrefix)
-{
-    if (productId === null
-        || productId === undefined
-        || (isNaN(productId) && isNaN(parseInt(productId)))
-        || index === null
-        || index === undefined
-        || (isNaN(index) && isNaN(parseInt(index)))) return;
-
-    searchStringPart = decodeHtmlString(searchStringPart);
-
-    removeSearchStringOriginDataSmallPopupCommon(
-        elementIdAndNamePrefix + "searchStringPartOrigin_li-" + productId + "-" + index,
-        elementIdAndNamePrefix + "searchStringPartOrigin_multipleOriginsDisplayList-" + productId + "-" + index,
-        searchStringPart,
-        elementIdAndNamePrefix + "searchStringPartOrigin_multipleOriginsDisplayList_nameLabel-" + productId + "-" + index,
-        elementIdAndNamePrefix +"searchStringPartOrigin_multipleOriginsDisplayList_meaningLabel-" + productId + "-" + index);
-}
-
 function addCopiedPartToTotalCopyAndMakeElementVisible(part, elementIds = null, makeElementVisible = false)
 {
     addCopiedPartToTotalCopy(part);
