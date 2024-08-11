414b3f77609a68771c9dbc0c64d431bc1a3a0715
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 22:05:07 +0300

    Changed ProductDisplayData.ImagesAndImageFileInfos to use ImageAndImageFileNameRelation class. Added MapToProductUpdateRequestWithoutImagesInDB method and moved several methods to MOSTComputers.Utils.ProductImageFileNameUtils.ProductImageFileNameUtils and ProductImageAndFileNameRelationsUtils classes.

--- MOSTPrices.UI.Web.RealWorkTesting/Utils/MappingUtils/ProductToDisplayDataMappingUtils.cs
+++ MOSTPrices.UI.Web.RealWorkTesting/Utils/MappingUtils/ProductToDisplayDataMappingUtils.cs
@@ -1,8 +1,10 @@
 ﻿using MOSTComputers.Models.Product.Models;
 using MOSTComputers.Models.Product.Models.ProductStatuses;
+using MOSTComputers.Services.ProductRegister.Models;
+using MOSTComputers.Services.ProductRegister.Models.Requests.Product;
 using MOSTComputers.UI.Web.RealWorkTesting.Models.Product;
-using OneOf;
-using OneOf.Types;
+using MOSTComputers.Utils.ProductImageFileNameUtils;
+using static MOSTComputers.Utils.ProductImageFileNameUtils.ProductImageAndFileNameRelationsUtils;
 
 namespace MOSTComputers.UI.Web.RealWorkTesting.Utils.MappingUtils;
 
@@ -82,11 +84,11 @@ public static class ProductToDisplayDataMappingUtils
             Manifacturer = productDisplayData.Manifacturer,
             SubCategoryId = productDisplayData.SubCategoryId,
             Images = productDisplayData.ImagesAndImageFileInfos?
-                .Select(x => x.Item1)
+                .Select(x => x.ProductImage)
                 .Where(x => x != null)!
                 .ToList<ProductImage>(),
             ImageFileNames = productDisplayData.ImagesAndImageFileInfos?
-                .Select(x => x.Item2)
+                .Select(x => x.ProductImageFileNameInfo)
                 .Where(x => x != null)!
                 .ToList<ProductImageFileNameInfo>(),
             Properties = productDisplayData.Properties?.ToList() ?? new(),
@@ -131,29 +133,29 @@ public static class ProductToDisplayDataMappingUtils
             ImagesAndImageFileInfos = productDisplayData.ImagesAndImageFileInfos?
             .Select(x =>
             {
-                bool imageIsNull = x.Item1 is not null;
-                bool imageFileNameIsNull = x.Item2 is not null;
+                bool imageIsNull = x.ProductImage is not null;
+                bool imageFileNameIsNull = x.ProductImageFileNameInfo is not null;
 
-                return new Tuple<ProductImage?, ProductImageFileNameInfo?>(
+                return new ImageAndImageFileNameRelation(
                     imageIsNull ?
                         new ProductImage()
                         {
-                            Id = x.Item1!.Id,
-                            ProductId = x.Item1.ProductId,
-                            ImageData = x.Item1.ImageData,
-                            ImageFileExtension = x.Item1.ImageFileExtension,
-                            HtmlData = x.Item1.HtmlData,
-                            DateModified = x.Item1.DateModified,
+                            Id = x.ProductImage!.Id,
+                            ProductId = x.ProductImage.ProductId,
+                            ImageData = x.ProductImage.ImageData,
+                            ImageContentType = x.ProductImage.ImageContentType,
+                            HtmlData = x.ProductImage.HtmlData,
+                            DateModified = x.ProductImage.DateModified,
                         }
                     : null,
                     imageFileNameIsNull ?
                         new ProductImageFileNameInfo()
                         {
-                            ProductId = x.Item2!.ProductId,
-                            ImageNumber = x.Item2.ImageNumber,
-                            DisplayOrder = x.Item2.DisplayOrder,
-                            Active = x.Item2.Active,
-                            FileName = x.Item2.FileName,
+                            ProductId = x.ProductImageFileNameInfo!.ProductId,
+                            ImageNumber = x.ProductImageFileNameInfo.ImageNumber,
+                            DisplayOrder = x.ProductImageFileNameInfo.DisplayOrder,
+                            Active = x.ProductImageFileNameInfo.Active,
+                            FileName = x.ProductImageFileNameInfo.FileName,
                         }
                     : null);
             })?
@@ -180,190 +182,125 @@ public static class ProductToDisplayDataMappingUtils
         };
     }
 
-    public static List<Tuple<ProductImage?, ProductImageFileNameInfo?>> GetImageDictionaryFromImagesAndImageFileInfos(
-        List<ProductImage>? productImages, List<ProductImageFileNameInfo>? productImageFileNameInfos)
+    public static ProductUpdateWithoutImagesInDatabaseRequest MapToProductUpdateRequestWithoutImagesInDB(ProductDisplayData productDisplayData)
     {
-        List<Tuple<ProductImage?, ProductImageFileNameInfo?>> output = new();
-
-        if (productImages != null)
+        return new()
         {
-            foreach (ProductImage image in productImages)
-            {
-                if (image.ImageFileExtension is null) continue;
+            Id = productDisplayData.Id,
+            Name = productDisplayData.Name,
+            AdditionalWarrantyPrice = productDisplayData.AdditionalWarrantyPrice,
+            AdditionalWarrantyTermMonths = productDisplayData.AdditionalWarrantyTermMonths,
+            StandardWarrantyPrice = productDisplayData.StandardWarrantyPrice,
+            StandardWarrantyTermMonths = productDisplayData.StandardWarrantyTermMonths,
+            DisplayOrder = productDisplayData.DisplayOrder,
+            Status = productDisplayData.Status,
+            PlShow = productDisplayData.PlShow,
+            Price1 = productDisplayData.Price1,
+            DisplayPrice = productDisplayData.DisplayPrice,
+            Price3 = productDisplayData.Price3,
+            Currency = productDisplayData.Currency,
+            RowGuid = productDisplayData.RowGuid,
+            PromotionId = productDisplayData.PromotionId,
+            PromRid = productDisplayData.PromRid,
+            PromotionPictureId = productDisplayData.PromotionPictureId,
+            PromotionExpireDate = productDisplayData.PromotionExpireDate,
+            AlertPictureId = productDisplayData.AlertPictureId,
+            AlertExpireDate = productDisplayData.AlertExpireDate,
+            PriceListDescription = productDisplayData.PriceListDescription,
+            PartNumber1 = productDisplayData.PartNumber1,
+            PartNumber2 = productDisplayData.PartNumber2,
+            SearchString = productDisplayData.SearchString,
+            Category = productDisplayData.Category,
+            Manifacturer = productDisplayData.Manifacturer,
+            SubCategoryId = productDisplayData.SubCategoryId,
 
-                string? imageFileName = GetImageFileNameFromImageData(image.Id, image.ImageFileExtension);
+            ImagesAndFileNames = productDisplayData.ImagesAndImageFileInfos?.ToList(),
 
-                if (imageFileName is null) continue;
+            Properties = productDisplayData.Properties?.ToList(),
 
-                ProductImageFileNameInfo? matchingProductImageFileNameInfo
-                    = productImageFileNameInfos?.FirstOrDefault(imageFileInfo => imageFileName == imageFileInfo.FileName);
-
-                output.Add(new(image, matchingProductImageFileNameInfo));
-
-                if (matchingProductImageFileNameInfo is not null)
-                {
-                    productImageFileNameInfos!.Remove(matchingProductImageFileNameInfo);
-                }
-            }
-        }
-
-        if (productImageFileNameInfos != null)
-        {
-            foreach (ProductImageFileNameInfo imageFileNameInfo in productImageFileNameInfos)
-            {
-                if (imageFileNameInfo.FileName is null) continue;
-
-                (string? imageIdOrTemporaryId, string? imageFileExtension) = GetImageDataFromImageFileName(imageFileNameInfo.FileName);
-
-                if (imageIdOrTemporaryId is null) continue;
-
-                bool isImageIdParseSuccessful = int.TryParse(imageIdOrTemporaryId, out int imageId);
-
-                if (!isImageIdParseSuccessful)
-                {
-                    output.Add(new(null, imageFileNameInfo));
-
-                    continue;
-                }
-
-                ProductImage? matchingProductImage = productImages?.FirstOrDefault(image => image.Id == imageId);
-
-                output.Add(new(matchingProductImage, imageFileNameInfo));
-            }
-        }
-
-        return OrderImagesAndImageFileNameInfos(output);
-    }
-
-    public static List<Tuple<ProductImage?, ProductImageFileNameInfo?>> OrderImagesAndImageFileNameInfos(
-        List<Tuple<ProductImage?, ProductImageFileNameInfo?>> imagesAndImageFileNameInfos)
-    {
-        Tuple<ProductImage?, ProductImageFileNameInfo?>[] output
-            = new Tuple<ProductImage?, ProductImageFileNameInfo?>[imagesAndImageFileNameInfos.Count];
-
-        for (int i = 0; i < imagesAndImageFileNameInfos.Count; i++)
-        {
-            Tuple<ProductImage?, ProductImageFileNameInfo?> tuple = imagesAndImageFileNameInfos[i];
-
-            ProductImage? image = tuple.Item1;
-            ProductImageFileNameInfo? imageFileNameInfo = tuple.Item2;
-
-            if (image is null
-                || imageFileNameInfo is null
-                || imageFileNameInfo.DisplayOrder is null) continue;
-
-            output[imageFileNameInfo.DisplayOrder.Value - 1] = tuple;
-
-            imagesAndImageFileNameInfos.Remove(tuple);
-
-            i--;
-        }
-
-        foreach (Tuple<ProductImage?, ProductImageFileNameInfo?> tuple in imagesAndImageFileNameInfos
-            .OrderBy(x => x.Item1?.Id ?? int.MaxValue))
-        {
-            for (int i = 0; i < output.Length; i++)
-            {
-                Tuple<ProductImage?, ProductImageFileNameInfo?> outputTuple = output[i];
-
-                if (outputTuple != null) continue;
-
-                output[i] = tuple;
-
-                break;
-            }
-        }
-
-        return output.ToList();
-    }
-
-    public static OneOf<int, (int, int), False> GetImageIdOrTempIdFromImageFileName(string fileName)
-    {
-        int endIndexOfIdOfImageFromNameOfFileInfo = fileName.IndexOf('.');
-
-        if (endIndexOfIdOfImageFromNameOfFileInfo < 0) return new False();
-
-        string idOfImageAsString = fileName[..endIndexOfIdOfImageFromNameOfFileInfo];
-
-        bool succeededGettingIdFromFileInfoName = int.TryParse(idOfImageAsString, out int idOfImage);
-
-        if (succeededGettingIdFromFileInfoName) return idOfImage;
-
-        int indexOfSeparatorOfTempId = idOfImageAsString.IndexOf('-');
-
-        if (indexOfSeparatorOfTempId < 0) return new False();
-
-        string productIdPartOfTempIdAsString = idOfImageAsString[..indexOfSeparatorOfTempId];
-
-        bool isProductIdParsed = int.TryParse(productIdPartOfTempIdAsString, out int productId);
-
-        if (!isProductIdParsed) return new False();
-
-        string imageNumberPartOfTempIdAsString = idOfImageAsString[(indexOfSeparatorOfTempId + 1)..];
-
-        bool isImageNumberParsed = int.TryParse(imageNumberPartOfTempIdAsString, out int imageNumber);
-
-        if (!isImageNumberParsed) return new False();
-
-        return (productId, imageNumber);
-    }
-
-    public static string? GetTemporaryIdFromFileNameInfoAndContentType(ProductImageFileNameInfo productImageFileNameInfo, string imageContentType)
-    {
-        if (productImageFileNameInfo.ProductId <= 0
-            || productImageFileNameInfo.ImageNumber <= 0
-            || string.IsNullOrWhiteSpace(imageContentType)) return null;
-
-        string? fileExtensionFromContentType = GetImageFileExtensionFromContentType(imageContentType);
-
-        if (fileExtensionFromContentType is null) return null;
-
-        return $"{productImageFileNameInfo.ProductId}-{productImageFileNameInfo.ImageNumber}.{fileExtensionFromContentType}";
-    }
-
-    public static string? GetImageFileNameFromImageData(int imageId, string imageContentType)
-    {
-        if (imageId < 0) return null;
-
-        string? fileExtension = GetImageFileExtensionFromContentType(imageContentType);
-
-        if (fileExtension is null) return null;
-
-        return $"{imageId}.{fileExtension}";
-    }
-
-    public static (string? imageIdOrTemporaryId, string? imageFileExtension) GetImageDataFromImageFileName(string imageFileName)
-    {
-        int indexOfDot = imageFileName.IndexOf('.');
-
-        if (indexOfDot < 0) return (null, null);
-
-        string imageIdOrTempId = imageFileName[..indexOfDot];
-
-        string imageFileExtension = imageFileName[(indexOfDot + 1)..];
-
-        return (imageIdOrTempId, imageFileExtension);
-    }
-
-    public static string? GetImageFileExtensionFromContentType(string imageContentType)
-    {
-        string? fileExtension = imageContentType switch
-        {
-            "image/jpeg" => "jpeg",
-            "image/jpg" => "jpg",
-            "image/png" => "png",
-            _ => null
+            ManifacturerId = (short?)productDisplayData.ManifacturerId,
+            CategoryId = productDisplayData.CategoryId,
         };
+    }
 
-        if (fileExtension is null)
+    public static ProductFullUpdateRequest MapToProductFullUpdateRequest(ProductDisplayData productDisplayData)
+    {
+        return new()
         {
-            int indexOfSlash = imageContentType.IndexOf('/');
+            Id = productDisplayData.Id,
+            Name = productDisplayData.Name,
+            AdditionalWarrantyPrice = productDisplayData.AdditionalWarrantyPrice,
+            AdditionalWarrantyTermMonths = productDisplayData.AdditionalWarrantyTermMonths,
+            StandardWarrantyPrice = productDisplayData.StandardWarrantyPrice,
+            StandardWarrantyTermMonths = productDisplayData.StandardWarrantyTermMonths,
+            DisplayOrder = productDisplayData.DisplayOrder,
+            Status = productDisplayData.Status,
+            PlShow = productDisplayData.PlShow,
+            Price1 = productDisplayData.Price1,
+            DisplayPrice = productDisplayData.DisplayPrice,
+            Price3 = productDisplayData.Price3,
+            Currency = productDisplayData.Currency,
+            RowGuid = productDisplayData.RowGuid,
+            PromotionId = productDisplayData.PromotionId,
+            PromRid = productDisplayData.PromRid,
+            PromotionPictureId = productDisplayData.PromotionPictureId,
+            PromotionExpireDate = productDisplayData.PromotionExpireDate,
+            AlertPictureId = productDisplayData.AlertPictureId,
+            AlertExpireDate = productDisplayData.AlertExpireDate,
+            PriceListDescription = productDisplayData.PriceListDescription,
+            PartNumber1 = productDisplayData.PartNumber1,
+            PartNumber2 = productDisplayData.PartNumber2,
+            SearchString = productDisplayData.SearchString,
+            Category = productDisplayData.Category,
+            Manifacturer = productDisplayData.Manifacturer,
+            SubCategoryId = productDisplayData.SubCategoryId,
 
-            if (indexOfSlash < 0) return null;
+            ImagesAndFileNames = productDisplayData.ImagesAndImageFileInfos?.ToList(),
 
-            return imageContentType[(indexOfSlash + 1)..];
-        }
+            Properties = productDisplayData.Properties?.ToList(),
 
-        return fileExtension;
+            ManifacturerId = (short?)productDisplayData.ManifacturerId,
+            CategoryId = productDisplayData.CategoryId,
+        };
+    }
+
+    public static ProductCreateWithoutImagesInDatabaseRequest MapToProductCreateRequestWithoutImagesInDB(ProductDisplayData productDisplayData)
+    {
+        return new()
+        {
+            Name = productDisplayData.Name,
+            AdditionalWarrantyPrice = productDisplayData.AdditionalWarrantyPrice,
+            AdditionalWarrantyTermMonths = productDisplayData.AdditionalWarrantyTermMonths,
+            StandardWarrantyPrice = productDisplayData.StandardWarrantyPrice,
+            StandardWarrantyTermMonths = productDisplayData.StandardWarrantyTermMonths,
+            DisplayOrder = productDisplayData.DisplayOrder,
+            Status = productDisplayData.Status,
+            PlShow = productDisplayData.PlShow,
+            Price1 = productDisplayData.Price1,
+            DisplayPrice = productDisplayData.DisplayPrice,
+            Price3 = productDisplayData.Price3,
+            Currency = productDisplayData.Currency,
+            RowGuid = productDisplayData.RowGuid,
+            PromotionId = productDisplayData.PromotionId,
+            PromRid = productDisplayData.PromRid,
+            PromotionPictureId = productDisplayData.PromotionPictureId,
+            PromotionExpireDate = productDisplayData.PromotionExpireDate,
+            AlertPictureId = productDisplayData.AlertPictureId,
+            AlertExpireDate = productDisplayData.AlertExpireDate,
+            PriceListDescription = productDisplayData.PriceListDescription,
+            PartNumber1 = productDisplayData.PartNumber1,
+            PartNumber2 = productDisplayData.PartNumber2,
+            SearchString = productDisplayData.SearchString,
+            Category = productDisplayData.Category,
+            Manifacturer = productDisplayData.Manifacturer,
+            SubCategoryId = productDisplayData.SubCategoryId,
+
+            ImagesAndFileNames = productDisplayData.ImagesAndImageFileInfos?.ToList(),
+
+            Properties = productDisplayData.Properties?.ToList(),
+
+            ManifacturerId = (short?)productDisplayData.ManifacturerId,
+            CategoryId = productDisplayData.CategoryId,
+        };
     }
 }
