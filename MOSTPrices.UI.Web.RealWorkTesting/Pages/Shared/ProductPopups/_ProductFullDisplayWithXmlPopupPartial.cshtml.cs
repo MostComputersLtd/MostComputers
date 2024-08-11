00f8e7d93dcc2bf72b1eddb2a5b2ae95734898d2
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 21:21:58 +0300

    Renamed _ProductFullDisplayWithXmlPopupPartialModel to ProductFullDisplayWithXmlPopupPartialModel.

--- MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/ProductPopups/_ProductFullDisplayWithXmlPopupPartial.cshtml.cs
+++ MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/ProductPopups/_ProductFullDisplayWithXmlPopupPartial.cshtml.cs
@@ -2,11 +2,9 @@ using MOSTComputers.Models.Product.Models;
 
 namespace MOSTComputers.UI.Web.RealWorkTesting.Pages.Shared.ProductPopups;
 
-#pragma warning disable IDE1006 // Naming Styles
-public sealed class _ProductFullDisplayWithXmlPopupPartialModel
-#pragma warning restore IDE1006 // Naming Styles
+public sealed class ProductFullDisplayWithXmlPopupPartialModel
 {
-    public _ProductFullDisplayWithXmlPopupPartialModel(Product product, string productXml, string productManifacturerSiteLink)
+    public ProductFullDisplayWithXmlPopupPartialModel(Product product, string productXml, string productManifacturerSiteLink)
     {
         Product = product;
         ProductXml = productXml;
