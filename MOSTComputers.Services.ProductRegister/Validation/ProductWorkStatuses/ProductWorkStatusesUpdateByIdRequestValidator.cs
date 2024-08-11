9e7edf98bb4a3560cf9a97f17c618985be65652d
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 18:53:00 +0300

    Init

---
+++ MOSTComputers.Services.ProductRegister/Validation/ProductWorkStatuses/ProductWorkStatusesUpdateByIdRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MOSTComputers.Models.Product.Models.Requests.ProductWorkStatuses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.ProductRegister.Validation.ProductWorkStatuses;
+internal sealed class ProductWorkStatusesUpdateByIdRequestValidator : AbstractValidator<ProductWorkStatusesUpdateByIdRequest>
+{
+    public ProductWorkStatusesUpdateByIdRequestValidator()
+    {
+        RuleFor(x => x.Id).NotNull().GreaterThan(0);
+    }
+}
---
+++ MOSTComputers.Services.ProductRegister/Validation/ProductWorkStatuses/ProductWorkStatusesUpdateByProductIdRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MOSTComputers.Models.Product.Models.Requests.ProductWorkStatuses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOSTComputers.Services.ProductRegister.Validation.ProductWorkStatuses;
+internal sealed class ProductWorkStatusesUpdateByProductIdRequestValidator : AbstractValidator<ProductWorkStatusesUpdateByProductIdRequest>
+{
+    public ProductWorkStatusesUpdateByProductIdRequestValidator()
+    {
+        RuleFor(x => x.ProductId).GreaterThan(0);
+    }
+}
