73c03506783cacc4ca4e5a051a4cc7fc2ed0f50a
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 18:56:22 +0300

    Init

---
+++ MOSTPrices.UI.Web.RealWorkTesting/wwwroot/js/Pages/Accounts/SignIn.js
@@ -0,0 +1,32 @@
+﻿function redirectToLogInPage(returnUrlInputElementId)
+{
+    var returnUrlInputElement = document.getElementById(returnUrlInputElementId);
+
+    if (returnUrlInputElement == null) return;
+
+    var returnUrl = returnUrlInputElement.value;
+
+    var url = "/Accounts/Signin" + "?handler=RedirectToLogInPage";
+
+    if (returnUrl != null
+        && returnUrl.length > 0)
+    {
+        url += "&returnUrl=" + returnUrl;
+    }
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        contentType: "application/json",
+        data: null,
+        headers:
+        {
+            RequestVerificationToken:
+                $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+    })
+        .done(function (result)
+        {
+            window.location.href = result.redirectUrl;
+        });
+}
