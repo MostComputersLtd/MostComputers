e82d019edc3b768d856e5dd760abc407b2a81c75
Daniel Nikolov <danielsnikolov@gmail.com>
2024-08-11 21:39:14 +0300

    Added arguments for all the popups that the _IndexProductTableRowPartial view uses. Added a second constructor that uses the ProductSelectListItemUtils class to generate all the select list item data.

--- MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/_IndexProductTableRowPartial.cshtml.cs
+++ MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/_IndexProductTableRowPartial.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MOSTComputers.Models.Product.Models;
 using MOSTComputers.UI.Web.RealWorkTesting.Models.Product;
+using MOSTComputers.UI.Web.RealWorkTesting.Utils;
 
 namespace MOSTComputers.UI.Web.RealWorkTesting.Pages.Shared;
 
@@ -14,7 +16,11 @@ public class IndexProductTableRowPartialModel
         IEnumerable<SelectListItem> statusSelectListItems,
         IEnumerable<SelectListItem> currencySelectListItems,
         IEnumerable<SelectListItem> productNewStatusSelectListItems,
-        IEnumerable<SelectListItem> productXmlStatusSelectListItems)
+        IEnumerable<SelectListItem> productXmlStatusSelectListItems,
+        string? productFullWithXmlPopupContentId = null,
+        string? productChangesPopupContentId = null,
+        string? productImagesPopupContentId = null,
+        string? productFirstImagePopupContentId = null)
     {
         ProductDisplayData = product;
         HtmlElementId = htmlElementId;
@@ -25,6 +31,39 @@ public class IndexProductTableRowPartialModel
         CurrencySelectListItems = currencySelectListItems;
         ProductNewStatusSelectListItems = productNewStatusSelectListItems;
         ProductXmlStatusSelectListItems = productXmlStatusSelectListItems;
+
+        ProductFullWithXmlPopupContentId = productFullWithXmlPopupContentId;
+        ProductChangesPopupContentId = productChangesPopupContentId;
+        ProductImagesPopupContentId = productImagesPopupContentId;
+        ProductFirstImagePopupContentId = productFirstImagePopupContentId;
+    }
+
+    public IndexProductTableRowPartialModel(
+        ProductDisplayData productData,
+        string htmlElementId,
+        int tableIndex,
+        IEnumerable<Category> allPossibleCategories,
+        IEnumerable<Manifacturer> allPossibleManifacturers,
+        string? productFullWithXmlPopupContentId = null,
+        string? productChangesPopupContentId = null,
+        string? productImagesPopupContentId = null,
+        string? productFirstImagePopupContentId = null)
+    {
+        ProductDisplayData = productData;
+        HtmlElementId = htmlElementId;
+        TableIndex = tableIndex;
+
+        CategorySelectListItems = ProductSelectListItemUtils.GetCategorySelectListItems(productData, allPossibleCategories);
+        ManifacturerSelectListItems = ProductSelectListItemUtils.GetManifacturerSelectListItems(productData, allPossibleManifacturers);
+        StatusSelectListItems = ProductSelectListItemUtils.GetStatusSelectListItems(productData);
+        CurrencySelectListItems = ProductSelectListItemUtils.GetCurrencySelectListItems(productData);
+        ProductNewStatusSelectListItems = ProductSelectListItemUtils.GetProductNewStatusSelectListItems(productData);
+        ProductXmlStatusSelectListItems = ProductSelectListItemUtils.GetProductXmlStatusSelectListItems(productData);
+
+        ProductFullWithXmlPopupContentId = productFullWithXmlPopupContentId;
+        ProductChangesPopupContentId = productChangesPopupContentId;
+        ProductImagesPopupContentId = productImagesPopupContentId;
+        ProductFirstImagePopupContentId = productFirstImagePopupContentId;
     }
 
     public ProductDisplayData ProductDisplayData { get; }
@@ -36,4 +75,9 @@ public class IndexProductTableRowPartialModel
     public IEnumerable<SelectListItem> CurrencySelectListItems { get; }
     public IEnumerable<SelectListItem> ProductNewStatusSelectListItems { get; }
     public IEnumerable<SelectListItem> ProductXmlStatusSelectListItems { get; }
+
+    public string? ProductFullWithXmlPopupContentId { get; }
+    public string? ProductChangesPopupContentId { get; }
+    public string? ProductImagesPopupContentId { get; }
+    public string? ProductFirstImagePopupContentId { get; }
 }
