6fab6d3bc50e57706094261987a22ffae9b9148a
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-26 13:50:58 +0300

    Commented out this service for now, logic was moved to ProductService.

--- MOSTComputers.Services.ProductRegister/Services/Contracts/IProductManipulateService.cs
+++ MOSTComputers.Services.ProductRegister/Services/Contracts/IProductManipulateService.cs
@@ -7,9 +7,10 @@ using OneOf;
 using OneOf.Types;
 
 namespace MOSTComputers.Services.ProductRegister.Services.Contracts;
-public interface IProductManipulateService
-{
-    Task<OneOf<int, ValidationResult, UnexpectedFailureResult, DirectoryNotFoundResult, FileDoesntExistResult>> InsertProductWithImagesOnlyInDirectoryAsync(ProductCreateWithoutImagesInDatabaseRequest productWithoutImagesInDBCreateRequest);
-    OneOf<Success, ValidationResult, UnexpectedFailureResult, NotSupportedFileTypeResult> UpdateProductFull(ProductFullUpdateRequest productFullUpdateRequest);
-    Task<OneOf<Success, ValidationResult, UnexpectedFailureResult, DirectoryNotFoundResult, FileDoesntExistResult>> UpdateProductWithoutSavingImagesInDBAsync(ProductUpdateWithoutImagesInDatabaseRequest productUpdateRequestWithoutImagesInDB);
-}
+
+//public interface IProductManipulateService
+//{
+//    Task<OneOf<int, ValidationResult, UnexpectedFailureResult, DirectoryNotFoundResult, FileDoesntExistResult>> InsertProductWithImagesOnlyInDirectoryAsync(ProductCreateWithoutImagesInDatabaseRequest productWithoutImagesInDBCreateRequest);
+//    OneOf<Success, ValidationResult, UnexpectedFailureResult, NotSupportedFileTypeResult> UpdateProductFull(ProductFullUpdateRequest productFullUpdateRequest);
+//    Task<OneOf<Success, ValidationResult, UnexpectedFailureResult, DirectoryNotFoundResult, FileDoesntExistResult>> UpdateProductWithoutSavingImagesInDBAsync(ProductUpdateWithoutImagesInDatabaseRequest productUpdateRequestWithoutImagesInDB);
+//}
