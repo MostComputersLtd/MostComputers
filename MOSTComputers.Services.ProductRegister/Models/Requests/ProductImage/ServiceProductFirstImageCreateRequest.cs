10a3d35437f7d709b59f067a8fe8ebd69bf535ac
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-26 12:33:05 +0300

    Changed ProductId to be not nullable

--- MOSTComputers.Services.ProductRegister/Models/Requests/ProductImage/ServiceProductFirstImageCreateRequest.cs
+++ MOSTComputers.Services.ProductRegister/Models/Requests/ProductImage/ServiceProductFirstImageCreateRequest.cs
@@ -2,7 +2,7 @@
 
 public sealed class ServiceProductFirstImageCreateRequest
 {
-    public int? ProductId { get; set; }
+    public int ProductId { get; set; }
     public string? HtmlData { get; set; }
     public byte[]? ImageData { get; set; }
     public string? ImageContentType { get; set; }
