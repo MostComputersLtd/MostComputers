478c98d370f53060f7ab1de51e88b19424b29cd9
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-26 12:36:55 +0300

    Changed ProductCharacteristicName to be not nullable. Renamed DisplayOrder to CustomDisplayOrder.

--- MOSTComputers.Models.Product/Models/Requests/ProductProperty/ProductPropertyByCharacteristicNameCreateRequest.cs
+++ MOSTComputers.Models.Product/Models/Requests/ProductProperty/ProductPropertyByCharacteristicNameCreateRequest.cs
@@ -9,8 +9,8 @@ namespace MOSTComputers.Models.Product.Models.Requests.ProductProperty;
 public sealed class ProductPropertyByCharacteristicNameCreateRequest
 {
     public int ProductId { get; set; }
-    public string ProductCharacteristicName { get; set; }
-    public int? DisplayOrder { get; set; }
+    public required string ProductCharacteristicName { get; set; }
+    public int? CustomDisplayOrder { get; set; }
     public string? Value { get; set; }
     public XMLPlacementEnum? XmlPlacement { get; set; }
 }
