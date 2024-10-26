237aac81a6e8a808be2f6aebba493ae8da065451
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-26 13:51:40 +0300

    Added GetByProductId method.

--- MOSTComputers.Services.ProductRegister/Services/Contracts/IProductWorkStatusesService.cs
+++ MOSTComputers.Services.ProductRegister/Services/Contracts/IProductWorkStatusesService.cs
@@ -9,19 +9,19 @@ using OneOf.Types;
 namespace MOSTComputers.Services.ProductRegister.Services.Contracts;
 public interface IProductWorkStatusesService
 {
-    bool DeleteAll();
-    bool DeleteByProductId(int productId);
-    bool DeleteAllWithProductNewStatus(ProductNewStatusEnum productNewStatusEnum);
-    bool DeleteAllWithProductXmlStatus(ProductXmlStatusEnum productXmlStatusEnum);
-    bool DeleteAllWithReadyForImageInsert(bool readyForImageInsert);
-    bool DeleteById(int productWorkStatusesId);
     IEnumerable<ProductWorkStatuses> GetAll();
-    ProductWorkStatuses? GetByProductId(int productId);
     IEnumerable<ProductWorkStatuses> GetAllWithProductNewStatus(ProductNewStatusEnum productNewStatusEnum);
     IEnumerable<ProductWorkStatuses> GetAllWithProductXmlStatus(ProductXmlStatusEnum productXmlStatusEnum);
     IEnumerable<ProductWorkStatuses> GetAllWithReadyForImageInsert(bool readyForImageInsert);
+    ProductWorkStatuses? GetByProductId(int productId);
     ProductWorkStatuses? GetById(int productWorkStatusesId);
     OneOf<int, ValidationResult, UnexpectedFailureResult> InsertIfItDoesntExist(ProductWorkStatusesCreateRequest createRequest, IValidator<ProductWorkStatusesCreateRequest>? validator = null);
     OneOf<bool, ValidationResult> UpdateByProductId(ProductWorkStatusesUpdateByProductIdRequest updateRequest, IValidator<ProductWorkStatusesUpdateByProductIdRequest>? validator = null);
     OneOf<bool, ValidationResult> UpdateById(ProductWorkStatusesUpdateByIdRequest updateRequest, IValidator<ProductWorkStatusesUpdateByIdRequest>? validator = null);
+    bool DeleteAll();
+    bool DeleteAllWithProductNewStatus(ProductNewStatusEnum productNewStatusEnum);
+    bool DeleteAllWithProductXmlStatus(ProductXmlStatusEnum productXmlStatusEnum);
+    bool DeleteAllWithReadyForImageInsert(bool readyForImageInsert);
+    bool DeleteByProductId(int productId);
+    bool DeleteById(int productWorkStatusesId);
 }
