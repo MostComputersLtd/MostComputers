91c6240afb8d5b9770b1e8b59f95df69822743b0
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-26 11:29:38 +0300

    Improved the performance of EscapeAmpersandsThatArentUsedToEscapeOtherCharacters, EscapeSmallerSignInSearchStrings and EscapeSmallerSignInProperties. Fixed TransformXmlDataBackToNormal to handle null values.

--- MOSTComputers.Services.HTMLAndXMLDataOperations/StaticUtils/ProductDeserializeXmlTransformUtils.cs
+++ MOSTComputers.Services.HTMLAndXMLDataOperations/StaticUtils/ProductDeserializeXmlTransformUtils.cs
@@ -1,4 +1,5 @@
 ﻿using MOSTComputers.Services.HTMLAndXMLDataOperations.Models;
+using System.Text;
 
 namespace MOSTComputers.Services.HTMLAndXMLDataOperations.StaticUtils;
 
@@ -10,8 +11,9 @@ internal static class ProductDeserializeXmlTransformUtils
     private const string _propertyTagStart = "<property";
     private const string _propertyTagEnd = "</property>";
 
-    private static readonly IReadOnlyList<string> _knownXmlEscapeKeywords = new List<string>()
+    private static readonly HashSet<string> _knownXmlEscapeKeywords = new()
     {
+        "&#",
         "&lt;",
         "&gt;",
         "&apos;",
@@ -28,17 +30,48 @@ internal static class ProductDeserializeXmlTransformUtils
         return xml;
     }
 
+    //private static string EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(string xml)
+    //{
+    //    for (int charIndex = 0; charIndex < xml.Length; charIndex++)
+    //    {
+    //        if (xml[charIndex] == '&')
+    //        {
+    //            bool ampersandIsAPartOfAnEscapeKeyword = false;
+
+    //            for (int k = 0; k < _knownXmlEscapeKeywords.Count; k++)
+    //            {
+    //                if (xml.IndexOf(_knownXmlEscapeKeywords[k], charIndex) == charIndex)
+    //                {
+    //                    ampersandIsAPartOfAnEscapeKeyword = true;
+
+    //                    break;
+    //                }
+    //            }
+
+    //            if (ampersandIsAPartOfAnEscapeKeyword) continue;
+
+    //            xml = xml[..charIndex] + "&amp;" + xml[(charIndex + 1)..];
+
+    //            charIndex += 4;
+    //        }
+    //    }
+
+    //    return xml;
+    //}
+
     private static string EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(string xml)
     {
+        StringBuilder output = new(xml.Length);
+
         for (int charIndex = 0; charIndex < xml.Length; charIndex++)
         {
             if (xml[charIndex] == '&')
             {
                 bool ampersandIsAPartOfAnEscapeKeyword = false;
 
-                for (int k = 0; k < _knownXmlEscapeKeywords.Count; k++)
+                foreach (string keyword in _knownXmlEscapeKeywords)
                 {
-                    if (xml.IndexOf(_knownXmlEscapeKeywords[k], charIndex) == charIndex)
+                    if (charIndex + keyword.Length <= xml.Length && xml.Substring(charIndex, keyword.Length) == keyword)
                     {
                         ampersandIsAPartOfAnEscapeKeyword = true;
 
@@ -46,25 +79,74 @@ internal static class ProductDeserializeXmlTransformUtils
                     }
                 }
 
-                if (ampersandIsAPartOfAnEscapeKeyword) continue;
+                if (!ampersandIsAPartOfAnEscapeKeyword)
+                {
+                    output.Append("&amp;");
 
-                xml = xml[..charIndex] + "&amp;" + xml[(charIndex + 1)..];
-
-                charIndex += 4;
+                    continue;
+                }
             }
+
+            output.Append(xml[charIndex]);
         }
 
-        return xml;
+        return output.ToString();
     }
+
+    //private static string EscapeSmallerSignInSearchStrings(string xml)
+    //{
+    //    int currentSearchStringIndex = 0;
+
+    //    const string searchStringTagStart = "<searchstring>";
+    //    const string searchStringTagEnd = "</searchstring>";
+
+    //    while (currentSearchStringIndex != -1)
+    //    {
+    //        int startIndexRaw = xml.IndexOf(searchStringTagStart, currentSearchStringIndex);
+    //        int endIndexRaw = xml.IndexOf(searchStringTagEnd, currentSearchStringIndex);
+
+    //        if (startIndexRaw == -1 || endIndexRaw == -1) break;
+
+    //        int searchStringStartIndex = startIndexRaw + searchStringTagStart.Length;
+    //        int searchStringEndIndex = endIndexRaw - 1;
+
+    //        int xmlStringChangeOffsetForSmallerSign = _smallerSignEscape.Length - 1;
+    //        int xmlStringChangeOffsetForBiggerSign = _biggerSignEscape.Length - 1;
+
+    //        for (int i = searchStringStartIndex; i < searchStringEndIndex; i++)
+    //        {
+    //            if (xml[i] == '<')
+    //            {
+    //                xml = xml[..i] + _smallerSignEscape + xml[(i + 1)..];
+
+    //                i += xmlStringChangeOffsetForSmallerSign;
+    //                searchStringEndIndex += xmlStringChangeOffsetForSmallerSign;
+    //            }
+    //            else if (xml[i] == '>')
+    //            {
+    //                xml = xml[..i] + _biggerSignEscape + xml[(i + 1)..];
+
+    //                i += xmlStringChangeOffsetForBiggerSign;
+    //                searchStringEndIndex += xmlStringChangeOffsetForBiggerSign;
+    //            }
+    //        }
+
+    //        currentSearchStringIndex = searchStringEndIndex + 19;
+    //    }
+
+    //    return xml;
+    //}
 
     private static string EscapeSmallerSignInSearchStrings(string xml)
     {
-        int currentSearchStringIndex = 0;
-
         const string searchStringTagStart = "<searchstring>";
         const string searchStringTagEnd = "</searchstring>";
 
-        while (currentSearchStringIndex != -1)
+        StringBuilder output = new(xml.Length);
+
+        int currentSearchStringIndex = 0;
+
+        while (true)
         {
             int startIndexRaw = xml.IndexOf(searchStringTagStart, currentSearchStringIndex);
             int endIndexRaw = xml.IndexOf(searchStringTagEnd, currentSearchStringIndex);
@@ -72,66 +154,119 @@ internal static class ProductDeserializeXmlTransformUtils
             if (startIndexRaw == -1 || endIndexRaw == -1) break;
 
             int searchStringStartIndex = startIndexRaw + searchStringTagStart.Length;
-            int searchStringEndIndex = endIndexRaw - 1;
+            int searchStringEndIndex = endIndexRaw;
 
-            int xmlStringChangeOffsetForSmallerSign = _smallerSignEscape.Length - 1;
-            int xmlStringChangeOffsetForBiggerSign = _biggerSignEscape.Length - 1;
+            output.Append(xml, currentSearchStringIndex, searchStringStartIndex - currentSearchStringIndex);
 
             for (int i = searchStringStartIndex; i < searchStringEndIndex; i++)
             {
                 if (xml[i] == '<')
                 {
-                    xml = xml[..i] + _smallerSignEscape + xml[(i + 1)..];
-
-                    i += xmlStringChangeOffsetForSmallerSign;
-                    searchStringEndIndex += xmlStringChangeOffsetForSmallerSign;
+                    output.Append(_smallerSignEscape);
                 }
                 else if (xml[i] == '>')
                 {
-                    xml = xml[..i] + _biggerSignEscape + xml[(i + 1)..];
-
-                    i += xmlStringChangeOffsetForBiggerSign;
-                    searchStringEndIndex += xmlStringChangeOffsetForBiggerSign;
+                    output.Append(_biggerSignEscape);
+                }
+                else
+                {
+                    output.Append(xml[i]);
                 }
             }
 
-            currentSearchStringIndex = searchStringEndIndex + 19;
+            output.Append(searchStringTagEnd);
+
+            currentSearchStringIndex = searchStringEndIndex + searchStringTagEnd.Length;
         }
 
-        return xml;
+        output.Append(xml, currentSearchStringIndex, xml.Length - currentSearchStringIndex);
+
+        return output.ToString();
     }
+
+    //private static string EscapeSmallerSignInProperties(string xml)
+    //{
+    //    int currentPropertyIndex = 0;
+
+    //    while (currentPropertyIndex != -1)
+    //    {
+    //        int startIndexRaw = xml.IndexOf(_propertyTagStart, currentPropertyIndex);
+    //        int endIndexRaw = xml.IndexOf(_propertyTagEnd, currentPropertyIndex);
+
+    //        if (startIndexRaw == -1 || endIndexRaw == -1) break;
+
+    //        int propertyStartIndex = startIndexRaw + 9;
+    //        int propertyEndIndex = endIndexRaw;
+
+    //        int xmlStringChangeOffset = _smallerSignEscape.Length - 1;
+
+    //        for (int i = propertyStartIndex; i < propertyEndIndex; i++)
+    //        {
+    //            if (xml[i] == '<')
+    //            {
+    //                xml = xml[..i] + _smallerSignEscape + xml[(i + 1)..];
+
+    //                i += xmlStringChangeOffset;
+    //                propertyEndIndex += xmlStringChangeOffset;
+    //            }
+    //        }
+
+    //        currentPropertyIndex = propertyEndIndex + _propertyTagEnd.Length + 1;
+    //    }
+
+    //    return xml;
+    //}
 
     private static string EscapeSmallerSignInProperties(string xml)
     {
+        StringBuilder output = new(xml.Length);
+
         int currentPropertyIndex = 0;
 
-        while (currentPropertyIndex != -1)
+        while (true)
         {
             int startIndexRaw = xml.IndexOf(_propertyTagStart, currentPropertyIndex);
-            int endIndexRaw = xml.IndexOf(_propertyTagEnd, currentPropertyIndex);
 
-            if (startIndexRaw == -1 || endIndexRaw == -1) break;
+            if (startIndexRaw == -1) break;
 
-            int propertyStartIndex = startIndexRaw + 9;
-            int propertyEndIndex = endIndexRaw;
+            int endIndexRaw = xml.IndexOf(_propertyTagEnd, startIndexRaw);
+            int selfClosingIndex = xml.IndexOf("/>", startIndexRaw);
 
-            int xmlStringChangeOffset = _smallerSignEscape.Length - 1;
+            bool isSelfClosing = selfClosingIndex != -1 && (endIndexRaw == -1 || selfClosingIndex < endIndexRaw);
+
+            int propertyStartIndex = startIndexRaw + _propertyTagStart.Length;
+            int propertyEndIndex = isSelfClosing ? selfClosingIndex : endIndexRaw;
+
+            // Append everything up to and including the <property ...> start tag
+            output.Append(xml, currentPropertyIndex, propertyStartIndex - currentPropertyIndex);
 
             for (int i = propertyStartIndex; i < propertyEndIndex; i++)
             {
                 if (xml[i] == '<')
                 {
-                    xml = xml[..i] + _smallerSignEscape + xml[(i + 1)..];
-
-                    i += xmlStringChangeOffset;
-                    propertyEndIndex += xmlStringChangeOffset;
+                    output.Append(_smallerSignEscape);
+                }
+                else
+                {
+                    output.Append(xml[i]);
                 }
             }
 
-            currentPropertyIndex = propertyEndIndex + _propertyTagEnd.Length + 1;
+            if (isSelfClosing)
+            {
+                output.Append("/>");
+                currentPropertyIndex = propertyEndIndex + 2;
+            }
+            else
+            {
+                output.Append(_propertyTagEnd);
+                currentPropertyIndex = propertyEndIndex + _propertyTagEnd.Length;
+            }
         }
 
-        return xml;
+        output.Append(xml, currentPropertyIndex, xml.Length - currentPropertyIndex);
+
+        return output.ToString();
     }
 
     private static string GetXmlWithEscapedCharacterWithinCertainTags(
@@ -206,9 +341,9 @@ internal static class ProductDeserializeXmlTransformUtils
 
             foreach (var property in item.XmlProductProperties)
             {
-                property.Name = property.Name.Replace(_smallerSignEscape, "<");
-                property.Value = property.Value.Replace(_smallerSignEscape, "<");
-                property.Order = property.Order.Replace(_smallerSignEscape, "<");
+                property.Name = property.Name?.Replace(_smallerSignEscape, "<") ?? string.Empty;
+                property.Value = property.Value?.Replace(_smallerSignEscape, "<") ?? string.Empty;
+                property.Order = property.Order?.Replace(_smallerSignEscape, "<") ?? string.Empty;
             }
         }
 
