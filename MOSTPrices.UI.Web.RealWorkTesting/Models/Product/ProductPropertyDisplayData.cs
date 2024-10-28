be03c4dd2af2aa37b36286e5cdbb0b174c106706
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-28 15:05:27 +0200

    Added to define a custom display for product properties.

---
+++ MOSTPrices.UI.Web.RealWorkTesting/Models/Product/ProductPropertyDisplayData.cs
@@ -0,0 +1,13 @@
+﻿using MOSTComputers.Models.Product.Models;
+
+namespace MOSTComputers.UI.Web.RealWorkTesting.Models.Product;
+
+public class ProductPropertyDisplayData
+{
+    public int? ProductCharacteristicId { get; set; }
+    public int? DisplayOrder { get; set; }
+    public int? CustomDisplayOrderOnInsertOrUpdate { get; set; }
+    public string? Characteristic { get; set; }
+    public string? Value { get; set; }
+    public XMLPlacementEnum? XmlPlacement { get; set; }
+}
