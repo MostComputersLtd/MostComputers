598ba89f265bef2fe69dd64fe85884bad5c7681e
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-28 15:18:49 +0200

    Added to display html data generated dynamically from the product.

---
+++ MOSTPrices.UI.Web.RealWorkTesting/wwwroot/js/Pages/Shared/ProductFullHtmlBasedDisplayPopupControl.js
@@ -0,0 +1,13 @@
+﻿function open_ProductFullHtmlBased_modal()
+{
+    let dialog = document.getElementById("ProductFullHtmlBased_modal_dialog");
+
+    dialog.style.height = window.innerHeight;
+
+    $("#ProductFullHtmlBased_modal").modal("show");
+}
+
+function close_ProductFullHtmlBased_modal()
+{
+    $("#ProductFullHtmlBased_modal").modal("toggle");
+}
