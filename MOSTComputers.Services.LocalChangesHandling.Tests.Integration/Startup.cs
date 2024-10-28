5c3bf7df328b3e11f8e4832f85a399e7edbdc9a2
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-28 12:58:57 +0200

    Added logic to retrieve the image directory relative and full path. Added ImageDirectoryRelativePath and ImageDirectoryFullPath properties to save that data.

--- MOSTComputers.Services.LocalChangesHandling.Tests.Integration/Startup.cs
+++ MOSTComputers.Services.LocalChangesHandling.Tests.Integration/Startup.cs
@@ -3,20 +3,56 @@ using System.Configuration;
 using MOSTComputers.Services.LocalChangesHandling.Configuration;
 using Respawn;
 using Respawn.Graph;
+using MOSTComputers.Services.ProductRegister.Configuration;
+using Microsoft.Extensions.Hosting;
 using static MOSTComputers.Services.LocalChangesHandling.Tests.Integration.CommonTestElements;
+using static MOSTComputers.Services.ProductImageFileManagement.Configuration.ConfigureServices;
+using static MOSTComputers.Services.HTMLAndXMLDataOperations.Configuration.ConfigureServices;
 
 namespace MOSTComputers.Services.LocalChangesHandling.Tests.Integration;
 
 public class Startup
 {
 #pragma warning disable CA1822 // Mark members as static
+    // Reason: If its static, then xUnit wont execute it
     public void ConfigureServices(IServiceCollection services)
 #pragma warning restore CA1822 // Mark members as static
     {
-        services.AddLocalChangesHandlingWithoutBackgroundService(ConnectionString);
+        string productImageFolderFilePath = ImageDirectoryRelativePath;
+
+        if (!Path.IsPathFullyQualified(productImageFolderFilePath))
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            IHostEnvironment hostEnvironment = serviceProvider.GetRequiredService<IHostEnvironment>();
+
+            int indexOfProjectName = hostEnvironment.ContentRootPath.IndexOf(hostEnvironment.ApplicationName);
+
+            if (indexOfProjectName < 0) throw new InvalidOperationException("Project name and root folder name do not match.");
+
+            int afterAppNameIndex = indexOfProjectName + hostEnvironment.ApplicationName.Length;
+
+            string projectFolderPath = hostEnvironment.ContentRootPath[..afterAppNameIndex];
+
+            productImageFolderFilePath = Path.GetFullPath(productImageFolderFilePath, projectFolderPath)
+                .Replace('\\', '/');
+
+            ImageDirectoryFullPath = productImageFolderFilePath;
+        }
+
+        services.AddProductImageFileManagement(productImageFolderFilePath);
+
+        services.AddProductHtmlService();
+
+        services.AddProductServices(ConnectionString);
+
+        services.AddLocalChangesHandlingWithoutBackgroundService();
+
     }
 
     internal static string ConnectionString { get; } = GetConnectionString();
+    internal static string ImageDirectoryRelativePath { get; } = GetImageDirectoryPath();
+    internal static string ImageDirectoryFullPath { get; private set; }
 
     internal static readonly RespawnerOptions RespawnerOptionsToIgnoreTablesThatShouldntBeWiped = new()
     {
@@ -41,5 +77,17 @@ public class Startup
         System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
 
         return config.ConnectionStrings.ConnectionStrings["Default"].ConnectionString;
+    }
+
+    private static string GetImageDirectoryPath()
+    {
+        ExeConfigurationFileMap configMap = new()
+        {
+            ExeConfigFilename = "./App.config"
+        };
+
+        System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+        return config.AppSettings.Settings["ProductImageDirectory"].Value;
     }
 }
--- MOSTComputers.Services.ProductRegister.Tests.Integration/Startup.cs
+++ MOSTComputers.Services.ProductRegister.Tests.Integration/Startup.cs
@@ -3,18 +3,48 @@ using MOSTComputers.Services.ProductRegister.Configuration;
 using Respawn.Graph;
 using Respawn;
 using System.Configuration;
+using Microsoft.Extensions.Hosting;
 using MOSTComputers.Services.Caching.Configuration;
 using static MOSTComputers.Services.Caching.Configuration.ConfigureServices;
+using static MOSTComputers.Services.ProductImageFileManagement.Configuration.ConfigureServices;
+using static MOSTComputers.Services.HTMLAndXMLDataOperations.Configuration.ConfigureServices;
 
 namespace MOSTComputers.Services.ProductRegister.Tests.Integration;
 
 public class Startup
 {
 #pragma warning disable CA1822 // Mark members as static
+    // Reason: If its static, then xUnit wont execute it
     public void ConfigureServices(IServiceCollection services)
 #pragma warning restore CA1822 // Mark members as static
     {
         services.AddMemoryCachingServices();
+
+        string productImageFolderFilePath = ImageDirectoryRelativePath;
+
+        if (!Path.IsPathFullyQualified(productImageFolderFilePath))
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            IHostEnvironment hostEnvironment = serviceProvider.GetRequiredService<IHostEnvironment>();
+
+            int indexOfProjectName = hostEnvironment.ContentRootPath.IndexOf(hostEnvironment.ApplicationName);
+
+            if (indexOfProjectName < 0) throw new InvalidOperationException("Project name and root folder name do not match.");
+
+            int afterAppNameIndex = indexOfProjectName + hostEnvironment.ApplicationName.Length;
+
+            string projectFolderPath = hostEnvironment.ContentRootPath[..afterAppNameIndex];
+
+            productImageFolderFilePath = Path.GetFullPath(productImageFolderFilePath, projectFolderPath)
+                .Replace('\\', '/');
+
+            ImageDirectoryFullPath = productImageFolderFilePath;
+        }
+
+        services.AddProductImageFileManagement(productImageFolderFilePath);
+
+        services.AddProductHtmlService();
 
         services.AddCachedProductServices(ConnectionString);
     }
@@ -25,6 +55,10 @@ public class Startup
     internal const string tableNameOfPromotionsTable = "Promotions";
 
     internal static string ConnectionString { get; } = GetConnectionString();
+    internal static string ImageDirectoryRelativePath { get; } = GetImageDirectoryPath();
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    internal static string ImageDirectoryFullPath { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     internal static readonly RespawnerOptions RespawnerOptionsToIgnoreTablesThatShouldntBeWiped = new()
     {
@@ -48,5 +82,17 @@ public class Startup
         System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
 
         return config.ConnectionStrings.ConnectionStrings["Default"].ConnectionString;
+    }
+
+    private static string GetImageDirectoryPath()
+    {
+        ExeConfigurationFileMap configMap = new()
+        {
+            ExeConfigFilename = "./App.config"
+        };
+
+        System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+        return config.AppSettings.Settings["ProductImageDirectory"].Value;
     }
 }
