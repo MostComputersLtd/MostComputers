0adbe619fa2b7e4c15c153bf7136a3e88e7e02f0
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-28 15:13:53 +0200

    Added to get and cache the latest external XML product data.

---
+++ MOSTPrices.UI.Web.RealWorkTesting/Services/Contracts/ExternalXmlImport/IProductXmlProvidingService.cs
@@ -0,0 +1,9 @@
+﻿using OneOf;
+using OneOf.Types;
+
+namespace MOSTComputers.UI.Web.RealWorkTesting.Services.Contracts.ExternalXmlImport;
+
+public interface IProductXmlProvidingService
+{
+    Task<OneOf<string, NotFound>> GetProductXmlAsync();
+}
---
+++ MOSTPrices.UI.Web.RealWorkTesting/Services/ExternalXmlImport/ProductXmlProvidingService.cs
@@ -0,0 +1,40 @@
+﻿using MOSTComputers.UI.Web.RealWorkTesting.Services.Contracts.ExternalXmlImport;
+using OneOf;
+using OneOf.Types;
+
+namespace MOSTComputers.UI.Web.RealWorkTesting.Services.ExternalXmlImport;
+
+public class ProductXmlProvidingService : IProductXmlProvidingService
+{
+    public ProductXmlProvidingService(IServiceScopeFactory serviceScopeFactory)
+    {
+       
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    private string? _productXml = null;
+
+    public async Task<OneOf<string, NotFound>> GetProductXmlAsync()
+    {
+        if (_productXml is null)
+        {
+            using IServiceScope scope = _serviceScopeFactory.CreateScope();
+
+            IConfiguration configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+            string? xmlDataPath = configuration.GetRequiredSection("ExternalDataPaths").GetValue<string>("AllProductsXmlEndpointPath");
+
+            if (xmlDataPath is null) return new NotFound();
+
+            IHttpClientFactory httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
+
+            using HttpClient client = httpClientFactory.CreateClient();
+
+            _productXml = await client.GetStringAsync(xmlDataPath);
+        }
+
+        return _productXml;
+    }
+}
