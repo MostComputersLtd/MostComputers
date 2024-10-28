17f2242e8f7c19f638cc3bb95e96b65a5a6aea6d
Daniel Nikolov <danielsnikolov@gmail.com>
2024-10-28 15:06:48 +0200

    Added to display html data generated dynamically from the product.

---
+++ MOSTPrices.UI.Web.RealWorkTesting/Pages/Shared/ProductPopups/_ProductFullHtmlBasedDisplayPopupPartial.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MOSTComputers.Models.Product.Models;
+using MOSTComputers.Services.HTMLAndXMLDataOperations.Models;
+using MOSTComputers.Services.HTMLAndXMLDataOperations.Services.Contracts;
+using MOSTComputers.Services.SearchStringOrigin.Models;
+using OneOf;
+
+namespace MOSTComputers.UI.Web.RealWorkTesting.Pages.Shared.ProductPopups;
+
+public class ProductFullHtmlBasedDisplayPopupPartialModel
+{
+    public required IProductHtmlService ProductHtmlService { get; init; }
+    public Product? Product { get; init; }
+    public List<Tuple<string, List<SearchStringPartOriginData>?>>? ProductSearchStringPartsAndDataAboutTheirOrigin { get; init; }
+
+    public OneOf<string?, InvalidXmlResult> GetProductHtml()
+    {
+        if (Product is null) return null;
+
+        OneOf<string, InvalidXmlResult> getProductHtmlResult = ProductHtmlService.TryGetHtmlFromProduct(Product);
+
+        return getProductHtmlResult!;
+    }
+}
