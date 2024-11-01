4a341cd23876b3cca3c3a975e1eb06c42e2c8889
Daniel Nikolov <danielsnikolov@gmail.com>
2024-11-01 16:09:21 +0200

    Renamed _biggerSignEscape to _greaterSignEscape, added optimized versions of EscapeSmallerSignInSearchStrings and EscapeSmallerSignInProperties using spans and commented out the old implementations.

--- MOSTComputers.Services.HTMLAndXMLDataOperations/StaticUtils/ProductDeserializeXmlTransformUtils.cs
+++ MOSTComputers.Services.HTMLAndXMLDataOperations/StaticUtils/ProductDeserializeXmlTransformUtils.cs
@@ -6,7 +6,7 @@ namespace MOSTComputers.Services.HTMLAndXMLDataOperations.StaticUtils;
 internal static class ProductDeserializeXmlTransformUtils
 {
     private const string _smallerSignEscape = "|smallerSign|";
-    private const string _biggerSignEscape = "|biggerSign|";
+    private const string _greaterSignEscape = "|biggerSign|";
 
     private const string _propertyTagStart = "<property";
     private const string _propertyTagEnd = "</property>";
@@ -21,45 +21,16 @@ internal static class ProductDeserializeXmlTransformUtils
         "&amp;"
     };
 
-    public static string TransformInputToWorkingXml(string xml)
+    public static string TransformInputToWorkingXml(ReadOnlySpan<char> xml)
     {
         xml = EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(xml);
         xml = EscapeSmallerSignInSearchStrings(xml);
         xml = EscapeSmallerSignInProperties(xml);
 
-        return xml;
+        return xml.ToString();
     }
 
-    //private static string EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(string xml)
-    //{
-    //    for (int charIndex = 0; charIndex < xml.Length; charIndex++)
-    //    {
-    //        if (xml[charIndex] == '&')
-    //        {
-    //            bool ampersandIsAPartOfAnEscapeKeyword = false;
-
-    //            for (int k = 0; k < _knownXmlEscapeKeywords.Count; k++)
-    //            {
-    //                if (xml.IndexOf(_knownXmlEscapeKeywords[k], charIndex) == charIndex)
-    //                {
-    //                    ampersandIsAPartOfAnEscapeKeyword = true;
-
-    //                    break;
-    //                }
-    //            }
-
-    //            if (ampersandIsAPartOfAnEscapeKeyword) continue;
-
-    //            xml = xml[..charIndex] + "&amp;" + xml[(charIndex + 1)..];
-
-    //            charIndex += 4;
-    //        }
-    //    }
-
-    //    return xml;
-    //}
-
-    private static string EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(string xml)
+    private static string EscapeAmpersandsThatArentUsedToEscapeOtherCharacters(ReadOnlySpan<char> xml)
     {
         StringBuilder output = new(xml.Length);
 
@@ -71,7 +42,7 @@ internal static class ProductDeserializeXmlTransformUtils
 
                 foreach (string keyword in _knownXmlEscapeKeywords)
                 {
-                    if (charIndex + keyword.Length <= xml.Length && xml.Substring(charIndex, keyword.Length) == keyword)
+                    if (charIndex + keyword.Length <= xml.Length && xml.Slice(charIndex, keyword.Length) == keyword)
                     {
                         ampersandIsAPartOfAnEscapeKeyword = true;
 
@@ -93,93 +64,58 @@ internal static class ProductDeserializeXmlTransformUtils
         return output.ToString();
     }
 
-    //private static string EscapeSmallerSignInSearchStrings(string xml)
-    //{
-    //    int currentSearchStringIndex = 0;
-
-    //    const string searchStringTagStart = "<searchstring>";
-    //    const string searchStringTagEnd = "</searchstring>";
-
-    //    while (currentSearchStringIndex != -1)
-    //    {
-    //        int startIndexRaw = xml.IndexOf(searchStringTagStart, currentSearchStringIndex);
-    //        int endIndexRaw = xml.IndexOf(searchStringTagEnd, currentSearchStringIndex);
-
-    //        if (startIndexRaw == -1 || endIndexRaw == -1) break;
-
-    //        int searchStringStartIndex = startIndexRaw + searchStringTagStart.Length;
-    //        int searchStringEndIndex = endIndexRaw - 1;
-
-    //        int xmlStringChangeOffsetForSmallerSign = _smallerSignEscape.Length - 1;
-    //        int xmlStringChangeOffsetForBiggerSign = _biggerSignEscape.Length - 1;
-
-    //        for (int i = searchStringStartIndex; i < searchStringEndIndex; i++)
-    //        {
-    //            if (xml[i] == '<')
-    //            {
-    //                xml = xml[..i] + _smallerSignEscape + xml[(i + 1)..];
-
-    //                i += xmlStringChangeOffsetForSmallerSign;
-    //                searchStringEndIndex += xmlStringChangeOffsetForSmallerSign;
-    //            }
-    //            else if (xml[i] == '>')
-    //            {
-    //                xml = xml[..i] + _biggerSignEscape + xml[(i + 1)..];
-
-    //                i += xmlStringChangeOffsetForBiggerSign;
-    //                searchStringEndIndex += xmlStringChangeOffsetForBiggerSign;
-    //            }
-    //        }
-
-    //        currentSearchStringIndex = searchStringEndIndex + 19;
-    //    }
-
-    //    return xml;
-    //}
-
-    private static string EscapeSmallerSignInSearchStrings(string xml)
+    private static string EscapeSmallerSignInSearchStrings(ReadOnlySpan<char> xmlSpan)
     {
         const string searchStringTagStart = "<searchstring>";
         const string searchStringTagEnd = "</searchstring>";
 
-        StringBuilder output = new(xml.Length);
+        StringBuilder output = new(xmlSpan.Length);
 
         int currentSearchStringIndex = 0;
 
         while (true)
         {
-            int startIndexRaw = xml.IndexOf(searchStringTagStart, currentSearchStringIndex);
-            int endIndexRaw = xml.IndexOf(searchStringTagEnd, currentSearchStringIndex);
+            int startIndexRaw = xmlSpan[currentSearchStringIndex..].IndexOf(searchStringTagStart);
 
-            if (startIndexRaw == -1 || endIndexRaw == -1) break;
+            if (startIndexRaw == -1) break;
 
-            int searchStringStartIndex = startIndexRaw + searchStringTagStart.Length;
-            int searchStringEndIndex = endIndexRaw;
+            startIndexRaw += currentSearchStringIndex;
 
-            output.Append(xml, currentSearchStringIndex, searchStringStartIndex - currentSearchStringIndex);
+            int endIndexRaw = xmlSpan[(startIndexRaw + searchStringTagStart.Length)..].IndexOf(searchStringTagEnd);
 
-            for (int i = searchStringStartIndex; i < searchStringEndIndex; i++)
+            if (endIndexRaw == -1) break;
+
+            endIndexRaw += startIndexRaw + searchStringTagStart.Length;
+
+            output.Append(xmlSpan[currentSearchStringIndex..startIndexRaw]);
+
+            output.Append(searchStringTagStart);
+
+            ReadOnlySpan<char> searchStringSpan = xmlSpan.Slice(startIndexRaw + searchStringTagStart.Length,
+                endIndexRaw - startIndexRaw - searchStringTagStart.Length);
+
+            foreach (char xmlChar in searchStringSpan)
             {
-                if (xml[i] == '<')
+                if (xmlChar == '<')
                 {
                     output.Append(_smallerSignEscape);
                 }
-                else if (xml[i] == '>')
+                else if (xmlChar == '>')
                 {
-                    output.Append(_biggerSignEscape);
+                    output.Append(_greaterSignEscape);
                 }
                 else
                 {
-                    output.Append(xml[i]);
+                    output.Append(xmlChar);
                 }
             }
 
             output.Append(searchStringTagEnd);
 
-            currentSearchStringIndex = searchStringEndIndex + searchStringTagEnd.Length;
+            currentSearchStringIndex = endIndexRaw + searchStringTagEnd.Length;
         }
 
-        output.Append(xml, currentSearchStringIndex, xml.Length - currentSearchStringIndex);
+        output.Append(xmlSpan[currentSearchStringIndex..]);
 
         return output.ToString();
     }
@@ -217,129 +153,133 @@ internal static class ProductDeserializeXmlTransformUtils
     //    return xml;
     //}
 
-    private static string EscapeSmallerSignInProperties(string xml)
+    //private static string EscapeSmallerSignInProperties(string xml)
+    //{
+    //    StringBuilder output = new(xml.Length);
+
+    //    int currentPropertyIndex = 0;
+
+    //    while (true)
+    //    {
+    //        int startIndexRaw = xml.IndexOf(_propertyTagStart, currentPropertyIndex);
+
+    //        if (startIndexRaw == -1) break;
+
+    //        int endIndexRaw = xml.IndexOf(_propertyTagEnd, startIndexRaw);
+    //        int selfClosingIndex = xml.IndexOf("/>", startIndexRaw);
+
+    //        bool isSelfClosing = selfClosingIndex != -1 && (endIndexRaw == -1 || selfClosingIndex < endIndexRaw);
+
+    //        int propertyStartIndex = startIndexRaw + _propertyTagStart.Length;
+    //        int propertyEndIndex = isSelfClosing ? selfClosingIndex : endIndexRaw;
+
+    //        // Append everything up to and including the <property ...> start tag
+    //        output.Append(xml, currentPropertyIndex, propertyStartIndex - currentPropertyIndex);
+
+    //        for (int i = propertyStartIndex; i < propertyEndIndex; i++)
+    //        {
+    //            if (xml[i] == '<')
+    //            {
+    //                output.Append(_smallerSignEscape);
+    //            }
+    //            else
+    //            {
+    //                output.Append(xml[i]);
+    //            }
+    //        }
+
+    //        if (isSelfClosing)
+    //        {
+    //            output.Append("/>");
+    //            currentPropertyIndex = propertyEndIndex + 2;
+    //        }
+    //        else
+    //        {
+    //            output.Append(_propertyTagEnd);
+    //            currentPropertyIndex = propertyEndIndex + _propertyTagEnd.Length;
+    //        }
+    //    }
+
+    //    output.Append(xml, currentPropertyIndex, xml.Length - currentPropertyIndex);
+
+    //    return output.ToString();
+    //}
+
+    private static string EscapeSmallerSignInProperties(ReadOnlySpan<char> xmlSpan)
     {
-        StringBuilder output = new(xml.Length);
+        const string selfClosingTagEnd = "/>";
+
+        StringBuilder output = new(xmlSpan.Length);
 
         int currentPropertyIndex = 0;
 
         while (true)
         {
-            int startIndexRaw = xml.IndexOf(_propertyTagStart, currentPropertyIndex);
+            int startIndexRaw = xmlSpan[currentPropertyIndex..].IndexOf(_propertyTagStart);
 
             if (startIndexRaw == -1) break;
 
-            int endIndexRaw = xml.IndexOf(_propertyTagEnd, startIndexRaw);
-            int selfClosingIndex = xml.IndexOf("/>", startIndexRaw);
+            startIndexRaw += currentPropertyIndex;
+
+            int endIndexRaw = xmlSpan[(startIndexRaw + _propertyTagStart.Length)..].IndexOf(_propertyTagEnd);
+
+            int selfClosingIndex = xmlSpan[startIndexRaw..].IndexOf(selfClosingTagEnd);
 
             bool isSelfClosing = selfClosingIndex != -1 && (endIndexRaw == -1 || selfClosingIndex < endIndexRaw);
 
-            int propertyStartIndex = startIndexRaw + _propertyTagStart.Length;
-            int propertyEndIndex = isSelfClosing ? selfClosingIndex : endIndexRaw;
-
-            // Append everything up to and including the <property ...> start tag
-            output.Append(xml, currentPropertyIndex, propertyStartIndex - currentPropertyIndex);
-
-            for (int i = propertyStartIndex; i < propertyEndIndex; i++)
+            if (isSelfClosing)
             {
-                if (xml[i] == '<')
+                endIndexRaw = startIndexRaw + selfClosingIndex + selfClosingTagEnd.Length;
+            }
+            else
+            {
+                endIndexRaw += startIndexRaw + _propertyTagStart.Length;
+            }
+
+            output.Append(xmlSpan[currentPropertyIndex..startIndexRaw]);
+            output.Append(xmlSpan[startIndexRaw..(startIndexRaw + _propertyTagStart.Length)]);
+
+            ReadOnlySpan<char> propertySpan = xmlSpan[(startIndexRaw + _propertyTagStart.Length)..endIndexRaw];
+
+            foreach (char xmlChar in propertySpan)
+            {
+                if (xmlChar == '<')
                 {
                     output.Append(_smallerSignEscape);
                 }
                 else
                 {
-                    output.Append(xml[i]);
+                    output.Append(xmlChar);
                 }
             }
 
             if (isSelfClosing)
             {
-                output.Append("/>");
-                currentPropertyIndex = propertyEndIndex + 2;
+                output.Append(selfClosingTagEnd);
+
+                currentPropertyIndex = endIndexRaw;
             }
             else
             {
                 output.Append(_propertyTagEnd);
-                currentPropertyIndex = propertyEndIndex + _propertyTagEnd.Length;
+
+                currentPropertyIndex = endIndexRaw + _propertyTagEnd.Length - _propertyTagStart.Length;
             }
         }
 
-        output.Append(xml, currentPropertyIndex, xml.Length - currentPropertyIndex);
+        output.Append(xmlSpan[currentPropertyIndex..]);
 
         return output.ToString();
     }
 
-    private static string GetXmlWithEscapedCharacterWithinCertainTags(
-        string xml, string xmlElementName, char charToEscape, string strToEscapeWith)
-    {
-        const string xmlEndTagWithoutValue = "/>";
-
-        int currentIndex = 0;
-
-        string startTag = $"<{xmlElementName}";
-        string endTag = $"</{xmlElementName}>";
-
-        while (currentIndex != -1)
-        {
-            int startIndexRaw = xml.IndexOf(startTag, currentIndex);
-            int endIndexRaw = xml.IndexOf(endTag, currentIndex);
-
-            if (startIndexRaw == -1) break;
-
-            int propertyStartIndex = startIndexRaw + startTag.Length;
-
-            if (endIndexRaw == -1)
-            {
-                string substringFromPropStart = xml[propertyStartIndex..];
-
-                if (substringFromPropStart.TrimStart().StartsWith(xmlEndTagWithoutValue))
-                {
-                    currentIndex = propertyStartIndex + xmlEndTagWithoutValue.Length;
-
-                    continue;
-                }
-
-                break;
-            }
-
-            int propertyEndIndex = endIndexRaw;
-
-            string substringBetweenTags = xml[propertyStartIndex..propertyEndIndex];
-
-            int indexOfEndForTagWithoutValue = substringBetweenTags.IndexOf(xmlEndTagWithoutValue);
-
-            if (indexOfEndForTagWithoutValue > -1)
-            {
-                propertyEndIndex = propertyStartIndex + indexOfEndForTagWithoutValue;
-            }
-
-            int escapeLengthChange = strToEscapeWith.Length - 1;
-
-            for (int i = propertyStartIndex; i < propertyEndIndex; i++)
-            {
-                if (xml[i] == charToEscape)
-                {
-                    xml = xml[..i] + strToEscapeWith + xml[(i + 1)..];
-
-                    i += escapeLengthChange;
-                    propertyEndIndex += escapeLengthChange;
-                }
-            }
-
-            currentIndex = propertyEndIndex + endTag.Length;
-        }
-
-        return xml;
-    }
-
     public static bool TransformXmlDataBackToNormal(XmlObjectData xmlData)
     {
-        foreach (XmlProduct item in xmlData.Products)
+        foreach (XmlProduct xmlProduct in xmlData.Products)
         {
-            item.SearchString = item.SearchString.Replace(_smallerSignEscape, "<");
-            item.SearchString = item.SearchString.Replace(_biggerSignEscape, ">");
+            xmlProduct.SearchString = xmlProduct.SearchString.Replace(_smallerSignEscape, "<");
+            xmlProduct.SearchString = xmlProduct.SearchString.Replace(_greaterSignEscape, ">");
 
-            foreach (var property in item.XmlProductProperties)
+            foreach (XmlProductProperty property in xmlProduct.XmlProductProperties)
             {
                 property.Name = property.Name?.Replace(_smallerSignEscape, "<") ?? string.Empty;
                 property.Value = property.Value?.Replace(_smallerSignEscape, "<") ?? string.Empty;
