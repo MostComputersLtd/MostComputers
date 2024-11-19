23d560a3f690a029602e98ecf047a6c46061ee88
Daniel Nikolov <danielsnikolov@gmail.com>
2024-11-19 15:42:46 +0200

    Added to abstract bulk save logic of XML data.

---
+++ MOSTPrices.UI.Web.RealWorkTesting/Services/Contracts/ExternalXmlImport/IProductXmlDataSaveService.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using OneOf;
+using OneOf.Types;
+using MOSTComputers.Models.FileManagement.Models;
+using MOSTComputers.Models.Product.Models.ExternalXmlImport;
+using MOSTComputers.Models.Product.Models.Validation;
+using MOSTComputers.Services.HTMLAndXMLDataOperations.Models;
+
+namespace MOSTComputers.UI.Web.RealWorkTesting.Services.Contracts.ExternalXmlImport;
+public interface IProductXmlDataSaveService
+{
+    OneOf<Success, ValidationResult, UnexpectedFailureResult> UpsertProductPropertiesFromXmlPropertiesBasedOnRelationData(IEnumerable<XmlProduct> xmlProducts, List<ProductCharacteristicAndExternalXmlDataRelation> characteristicAndPropertyRelations);
+    Task<OneOf<Success, ValidationResult, UnexpectedFailureResult, NotSupportedFileTypeResult, DirectoryNotFoundResult>> UpsertImageFileNamesAndFilesFromXmlDataAsync(IEnumerable<XmlProduct> xmlProducts, bool insertFiles = false);
+    Task<OneOf<Success, ValidationResult, InvalidXmlResult, UnexpectedFailureResult>> UpsertTestingImagesFromXmlDataAsync(IEnumerable<XmlProduct> xmlProducts);
+}
