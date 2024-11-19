4b2aaf863786baa8bedec8cd01ebf90a4ea2e13f
Daniel Nikolov <danielsnikolov@gmail.com>
2024-11-19 15:39:14 +0200

    Added ExecuteActionInTransaction, ExecuteActionInTransactionAndCommitWithCondition and ExecuteActionInTransactionAndCommitWithConditionAsync overloads that allow for the TransactionScopeOption and TransactionOptions of the TransactionScope that is created to be specified.

--- MOSTComputers.Services.ProductRegister/Services/Contracts/ITransactionExecuteService.cs
+++ MOSTComputers.Services.ProductRegister/Services/Contracts/ITransactionExecuteService.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Transactions;
+
 namespace MOSTComputers.Services.ProductRegister.Services.Contracts;
 
 public interface ITransactionExecuteService
@@ -7,8 +9,16 @@ public interface ITransactionExecuteService
     void ExecuteActionInTransaction<T>(Action<T> action, T parameter);
     TReturn ExecuteActionInTransaction<TReturn>(Func<TReturn> action);
     TReturn ExecuteActionInTransaction<T, TReturn>(Func<T, TReturn> action, T parameter);
+    void ExecuteActionInTransaction(Action action, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
+    void ExecuteActionInTransaction<T>(Action<T> action, T parameter, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
+    TReturn ExecuteActionInTransaction<TReturn>(Func<TReturn> action, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
+    TReturn ExecuteActionInTransaction<T, TReturn>(Func<T, TReturn> action, T parameter, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
     TReturn ExecuteActionInTransactionAndCommitWithCondition<TReturn>(Func<TReturn> action, Predicate<TReturn> shouldCommit);
     TReturn ExecuteActionInTransactionAndCommitWithCondition<T, TReturn>(Func<T, TReturn> action, Predicate<TReturn> shouldCommit, T parameter);
+    TReturn ExecuteActionInTransactionAndCommitWithCondition<TReturn>(Func<TReturn> action, Predicate<TReturn> shouldCommit, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
+    TReturn ExecuteActionInTransactionAndCommitWithCondition<T, TReturn>(Func<T, TReturn> action, Predicate<TReturn> shouldCommit, T parameter, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
     Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<TReturn>(Func<Task<TReturn>> action, Predicate<TReturn> shouldCommit);
     Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<T, TReturn>(Func<T, Task<TReturn>> action, Predicate<TReturn> shouldCommit, T parameter);
+    Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<TReturn>(Func<Task<TReturn>> action, Predicate<TReturn> shouldCommit, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
+    Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<T, TReturn>(Func<T, Task<TReturn>> action, Predicate<TReturn> shouldCommit, T parameter, TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required, TransactionOptions? transactionOptions = null);
 }
--- MOSTComputers.Services.ProductRegister/Services/TransactionExecuteService.cs
+++ MOSTComputers.Services.ProductRegister/Services/TransactionExecuteService.cs
@@ -1,5 +1,6 @@
 ﻿using MOSTComputers.Services.DAL.DAL;
 using MOSTComputers.Services.ProductRegister.Services.Contracts;
+using System.Transactions;
 
 namespace MOSTComputers.Services.ProductRegister.Services;
 
@@ -17,9 +18,31 @@ internal sealed class TransactionExecuteService : ITransactionExecuteService
         _relationalDataAccess.SaveDataInTransactionScopeUsingAction(action);
     }
 
+    public void ExecuteActionInTransaction(Action action,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        action();
+
+        scope.Complete();
+    }
+
     public void ExecuteActionInTransaction<T>(Action<T> action, T parameter)
     {
         _relationalDataAccess.SaveDataInTransactionScopeUsingAction(action, parameter);
+    }
+
+    public void ExecuteActionInTransaction<T>(Action<T> action, T parameter,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        action(parameter);
+
+        scope.Complete();
     }
 
     public TReturn ExecuteActionInTransaction<TReturn>(Func<TReturn> action)
@@ -27,9 +50,35 @@ internal sealed class TransactionExecuteService : ITransactionExecuteService
         return _relationalDataAccess.SaveDataInTransactionScopeUsingAction(action);
     }
 
+    public TReturn ExecuteActionInTransaction<TReturn>(Func<TReturn> action,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        TReturn data = action();
+
+        scope.Complete();
+
+        return data;
+    }
+
     public TReturn ExecuteActionInTransaction<T, TReturn>(Func<T, TReturn> action, T parameter)
     {
         return _relationalDataAccess.SaveDataInTransactionScopeUsingAction(action, parameter);
+    }
+
+    public TReturn ExecuteActionInTransaction<T, TReturn>(Func<T, TReturn> action, T parameter,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        TReturn data = action(parameter);
+
+        scope.Complete();
+
+        return data;
     }
 
     public TReturn ExecuteActionInTransactionAndCommitWithCondition<TReturn>(Func<TReturn> action, Predicate<TReturn> shouldCommit)
@@ -37,18 +86,91 @@ internal sealed class TransactionExecuteService : ITransactionExecuteService
         return _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnCondition(action, shouldCommit);
     }
 
+    public TReturn ExecuteActionInTransactionAndCommitWithCondition<TReturn>(Func<TReturn> action, Predicate<TReturn> shouldCommit,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        TReturn data = action();
+
+        if (shouldCommit(data))
+        {
+            scope.Complete();
+        }
+
+        return data;
+    }
+
     public TReturn ExecuteActionInTransactionAndCommitWithCondition<T, TReturn>(Func<T, TReturn> action, Predicate<TReturn> shouldCommit, T parameter)
     {
         return _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnCondition(action, shouldCommit, parameter);
     }
 
-    public Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<TReturn>(Func<Task<TReturn>> action, Predicate<TReturn> shouldCommit)
+    public TReturn ExecuteActionInTransactionAndCommitWithCondition<T, TReturn>(Func<T, TReturn> action,
+        Predicate<TReturn> shouldCommit,
+        T parameter,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
     {
-        return _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnConditionAsync(action, shouldCommit);
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption) : new(transactionScopeOption, transactionOptions.Value);
+
+        TReturn data = action(parameter);
+
+        if (shouldCommit(data))
+        {
+            scope.Complete();
+        }
+
+        return data;
     }
 
-    public Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<T, TReturn>(Func<T, Task<TReturn>> action, Predicate<TReturn> shouldCommit, T parameter)
+    public async Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<TReturn>(Func<Task<TReturn>> action, Predicate<TReturn> shouldCommit)
     {
-        return _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnConditionAsync(action, shouldCommit, parameter);
+        return await _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnConditionAsync(action, shouldCommit);
+    }
+
+    public async Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<TReturn>(Func<Task<TReturn>> action,
+        Predicate<TReturn> shouldCommit,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption, TransactionScopeAsyncFlowOption.Enabled)
+            : new(transactionScopeOption, transactionOptions.Value, TransactionScopeAsyncFlowOption.Enabled);
+
+        TReturn data = await action();
+
+        if (shouldCommit(data))
+        {
+            scope.Complete();
+        }
+
+        return data;
+    }
+
+    public async Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<T, TReturn>(
+        Func<T, Task<TReturn>> action, Predicate<TReturn> shouldCommit, T parameter)
+    {
+        return await _relationalDataAccess.SaveDataInTransactionScopeUsingActionAndCommitOnConditionAsync(action, shouldCommit, parameter);
+    }
+
+    public async Task<TReturn> ExecuteActionInTransactionAndCommitWithConditionAsync<T, TReturn>(
+        Func<T, Task<TReturn>> action,
+        Predicate<TReturn> shouldCommit,
+        T parameter,
+        TransactionScopeOption transactionScopeOption = TransactionScopeOption.Required,
+        TransactionOptions? transactionOptions = null)
+    {
+        using TransactionScope scope = (transactionOptions is null) ? new(transactionScopeOption, TransactionScopeAsyncFlowOption.Enabled)
+            : new(transactionScopeOption, transactionOptions.Value, TransactionScopeAsyncFlowOption.Enabled);
+
+        TReturn data = await action(parameter);
+
+        if (shouldCommit(data))
+        {
+            scope.Complete();
+        }
+
+        return data;
     }
 }
