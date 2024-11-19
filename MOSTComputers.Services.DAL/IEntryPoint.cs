0e9bb123e35dd75bd04215be553287d6b604e262
Daniel Nikolov <danielsnikolov@gmail.com>
2024-11-19 15:21:43 +0200

    Removed unnecessary usings.

--- MOSTComputers.Services.DAL/IEntryPoint.cs
+++ MOSTComputers.Services.DAL/IEntryPoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MOSTComputers.Services.DAL;
+﻿namespace MOSTComputers.Services.DAL;
 
 public interface IEntryPoint
 {
--- MOSTComputers.Services.ProductRegister/IEntryPoint.cs
+++ MOSTComputers.Services.ProductRegister/IEntryPoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MOSTComputers.Services.ProductRegister;
+﻿namespace MOSTComputers.Services.ProductRegister;
 
 public interface IEntryPoint
 {
