9f870f7d07012812250608635c63620d073c9a87
Daniel Nikolov <danielsnikolov@gmail.com>
2024-12-13 13:39:14 +0200

    Made CreateInvoicePdf async and made it directly save the file, without returning document.

--- MOSTComputers.Services.PDF/Services/Contracts/IPdfInvoiceService.cs
+++ MOSTComputers.Services.PDF/Services/Contracts/IPdfInvoiceService.cs
@@ -4,5 +4,5 @@ using Spire.Pdf;
 namespace MOSTComputers.Services.PDF.Services.Contracts;
 public interface IPdfInvoiceService
 {
-    PdfDocument CreateInvoicePdf(InvoiceData invoiceData, string destinationFilePath);
+    Task CreateInvoicePdf(InvoiceData invoiceData, string destinationFilePath);
 }
