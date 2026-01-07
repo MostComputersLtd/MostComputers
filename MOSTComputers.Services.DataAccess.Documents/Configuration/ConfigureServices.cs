7822f57e5b6754a1d14cf20203f8328b4db0646a
Most Computers <mostltddeveloper@gmail.com>
2026-01-07 18:32:54 +0200

    Moved customer data logic to MOSTComputers.Services.Identity

--- MOSTComputers.Services.DataAccess.Documents/Configuration/ConfigureServices.cs
+++ MOSTComputers.Services.DataAccess.Documents/Configuration/ConfigureServices.cs
@@ -24,8 +24,6 @@ public static class ConfigureServices
         services.TryAddScoped<IWarrantyCardItemRepository, WarrantyCardItemRepository>();
         services.TryAddScoped<IWarrantyCardRepository, WarrantyCardRepository>();
 
-        services.TryAddScoped<ICustomerDataRepository, CustomerDataRepository>();
-
         return services;
     }
 
@@ -40,8 +38,6 @@ public static class ConfigureServices
             config.AddMap(new WarrantyCardItemDAOEntityMap());
             config.AddMap(new WarrantyCardDAOEntityMap());
             config.AddMap(new WarrantyCardCustomerDataEntityMap());
-
-            config.AddMap(new CustomerDataEntityMap());
         });
     }
 }
--- MOSTComputers.Services.DataAccess.Documents/DataAccess/Contracts/ICustomerDataRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MOSTComputers.Services.DataAccess.Documents.Models;
-
-namespace MOSTComputers.Services.DataAccess.Documents.DataAccess.Contracts;
-public interface ICustomerDataRepository
-{
-    Task<List<CustomerData>> GetAllAsync();
-    Task<CustomerData?> GetByIdAsync(int id);
-    Task<List<CustomerData>> GetByIdsAsync(List<int> ids);
-}
