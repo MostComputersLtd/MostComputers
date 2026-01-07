f04c8d04cccdf2c8f981af03aa9bfd8239071083
Most Computers <mostltddeveloper@gmail.com>
2026-01-07 18:43:14 +0200

    Changed popup to use <dialog> HTML tag for native focus trap.

---
+++ MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor.Client/Popup.razor.js
@@ -0,0 +1,48 @@
+﻿export function openDialog(dialogId, asModal = true) {
+    const dialog = document.getElementById(dialogId);
+
+    if (dialog == null) return;
+
+    if (asModal) {
+
+        dialog.showModal();
+
+        return;
+    }
+
+    dialog.show();
+}
+
+export function closeDialog(dialogId) {
+    const dialog = document.getElementById(dialogId);
+
+    if (dialog == null) return;
+
+    dialog.close();
+}
+
+async function overrideDefaultCloseEvent(e) {
+    e.preventDefault();
+
+    await e.target._customDotNetRef.invokeMethodAsync("OnDefaultCloseAsync");
+}
+
+export async function attachAutoCloseEvent(dialogId, dotNetRef) {
+    const dialog = document.getElementById(dialogId);
+
+    if (dialog == null) return;
+
+    dialog._customDotNetRef = dotNetRef;
+
+    dialog.addEventListener("cancel", overrideDefaultCloseEvent);
+}
+
+export async function detachAutoCloseEvent(dialogId) {
+    const dialog = document.getElementById(dialogId);
+
+    if (dialog == null) return;
+
+    dialog.removeEventListener("cancel", overrideDefaultCloseEvent);
+
+    dialog._customDotNetRef = null;
+}
