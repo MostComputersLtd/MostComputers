6dcc7fe56f1b1e98fe5669f87cc902664b654a86
Most Computers <mostltddeveloper@gmail.com>
2026-01-14 18:09:32 +0200

    Added a bulk insert method to IProductWorkStatusesRepository

---
+++ MOSTComputers.Services.DataAccess.Products/Models/Requests/ProductWorkStatuses/ProductWorkStatusesCreateManyWithSameDataRequest.cs
@@ -0,0 +1,15 @@
+﻿using MOSTComputers.Models.Product.Models.ProductStatuses;
+
+namespace MOSTComputers.Services.DataAccess.Products.Models.Requests.ProductWorkStatuses;
+
+public sealed class ProductWorkStatusesCreateManyWithSameDataRequest
+{
+    public required List<int> ProductIds { get; set; }
+    public required ProductNewStatus ProductNewStatus { get; set; }
+    public required ProductXmlStatus ProductXmlStatus { get; set; }
+    public required bool ReadyForImageInsert { get; set; }
+    public required string CreateUserName { get; init; }
+    public required DateTime CreateDate { get; init; }
+    public required string LastUpdateUserName { get; init; }
+    public required DateTime LastUpdateDate { get; init; }
+}
---
+++ MOSTComputers.Services.DataAccess.Products/Models/Responses/ProductWorkStatuses/ProductWorkStatusesCreateManyWithSameDataResponse.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation.Results;
+using MOSTComputers.Models.Product.Models.Validation;
+using OneOf;
+
+namespace MOSTComputers.Services.DataAccess.Products.Models.Responses.ProductWorkStatuses;
+public sealed class ProductWorkStatusesCreateManyWithSameDataResponse
+{
+    public required Dictionary<int, OneOf<int, ValidationResult, UnexpectedFailureResult>> Results { get; init; }
+}
