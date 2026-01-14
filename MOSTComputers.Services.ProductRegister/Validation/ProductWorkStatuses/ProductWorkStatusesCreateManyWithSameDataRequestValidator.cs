7bba442dc9c2cb43e3fd3f0e652f6e9ccbcaa2ad
Most Computers <mostltddeveloper@gmail.com>
2026-01-14 18:15:59 +0200

    Added new validator for bulk inserts

---
+++ MOSTComputers.Services.ProductRegister/Validation/ProductWorkStatuses/ProductWorkStatusesCreateManyWithSameDataRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MOSTComputers.Services.ProductRegister.Models.Requests.ProductWorkStatuses;
+
+namespace MOSTComputers.Services.ProductRegister.Validation.ProductWorkStatuses;
+
+internal class ProductWorkStatusesCreateManyWithSameDataRequestValidator : AbstractValidator<ServiceProductWorkStatusesCreateManyWithSameDataRequest>
+{
+    public ProductWorkStatusesCreateManyWithSameDataRequestValidator()
+    {
+        RuleFor(x => x.ProductIds).NotEmpty();
+        RuleFor(x => x.ProductIds).ForEach(x => x.NotNull().GreaterThan(0));
+        RuleFor(x => x.CreateUserName).NotNullOrWhiteSpace();
+    }
+}
