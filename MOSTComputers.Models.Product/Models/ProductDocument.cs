c47517141550988a7d7047f25b858c10ccf5dc19
Most Computers <mostltddeveloper@gmail.com>
2026-03-18 18:29:56 +0200

    Added ProductDocument model.

---
+++ MOSTComputers.Models.Product/Models/ProductDocument.cs
@@ -0,0 +1,8 @@
+﻿namespace MOSTComputers.Models.Product.Models;
+public sealed class ProductDocument
+{
+    public int Id { get; init; }
+    public int ProductId { get; init; }
+    public required string FileName { get; init; }
+    public string? Description { get; init; }
+}
