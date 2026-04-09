8db7db9dafca029609705e1cae3c9deb1d140e14
Daniel Nikolov <danielsnikolov@gmail.com>
2026-04-09 19:05:30 +0300

    Added a JS keyboard event to make Enter key redirect to search input.

--- MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/Components/Pages/ProductEditor.razor.js
+++ MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/Components/Pages/ProductEditor.razor.js
@@ -1,4 +1,88 @@
-﻿function toggleElementDisplay(elementId)
+﻿const searchInputId = "userInput";
+
+export function registerKeyDownEvent() {
+   
+    attachKeyDownEvent();
+
+    console.log(Blazor.addEventListener);
+
+    Blazor.addEventListener("enhancedload", removeKeyDownEvent);
+    window.addEventListener("beforeunload", removeKeyDownEvent);
+}
+
+function attachKeyDownEvent() {
+    const root = document.getElementById(pageRootDivId);
+
+    if (!root || root._listenerAttached) return;
+
+    window.addEventListener("keydown", MakeEnterRedirectToSearchInput);
+
+    root._listenerAttached = true;
+
+    console.log("Attached");
+}
+
+function removeKeyDownEvent() {
+    const root = document.getElementById(pageRootDivId);
+
+    window.removeEventListener("keydown", MakeEnterRedirectToSearchInput);
+
+    if (root) {
+        root._listenerAttached = false;
+    }
+
+    console.log("Removed");
+}
+
+function MakeEnterRedirectToSearchInput(e) {
+    if (e.key !== 'Enter') return;
+
+    console.log("EVENT");
+
+    const pageRootElement = document.getElementById(pageRootDivId);
+    const searchInput = document.getElementById(searchInputId);
+
+    if (!pageRootElement || !searchInput) return;
+
+    const el = document.activeElement;
+
+    if (!el) return;
+
+    const elementTag = el.tagName.toLowerCase();
+    const elementType = (el.getAttribute('type') || '').toLowerCase();
+
+    const nativeInteractiveInputTypes = ['button', 'submit', 'checkbox', 'radio'];
+
+    const isNativeInteractiveElement =
+        elementTag === 'button' ||
+        elementTag === 'textarea' ||
+        elementTag === 'select' ||
+        elementTag === 'a' ||
+        (elementTag === 'input' && nativeInteractiveInputTypes.includes(elementType));
+
+    const isContentEditable = el.isContentEditable;
+    const isRoleButton = el.getAttribute('role') === 'button';
+
+    if (isNativeInteractiveElement || isContentEditable || isRoleButton) {
+        return;
+    }
+
+    const isThereAnyOpenDialog = Array.from(document.querySelectorAll('dialog')).some(
+        dialogElement => dialogElement.open
+    );
+
+    if (isThereAnyOpenDialog) {
+        return;
+    }
+
+    e.preventDefault();
+
+    console.log("Prevented default");
+
+    searchInput.focus();
+}
+
+function toggleElementDisplay(elementId)
 {
     const element = document.getElementById(elementId);
 
@@ -70,3 +154,5 @@ export function openFileDataInNewWindow(fileData, contentType, newWindowTitle)
         newWindow.document.title = newWindowTitle;
     }
 }
+
+const pageRootDivId = "productEditorPage";
