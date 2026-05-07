7673f3dbc9ce104f1bea53d363fffbb2cc797407
Most Computers <mostltddeveloper@gmail.com>
2026-05-07 18:23:41 +0300

    Added a file versioning system.

---
+++ MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/Services/BrowserResourceHashing/WebResourceUrlsHashingService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Security.Cryptography;
+
+namespace MOSTComputers.UI.Web.Blazor.Services.BrowserResourceHashing;
+
+public class WebResourceUrlsHashingService
+{
+    public string GetLocalFileHash(string filePath)
+    {
+        using FileStream stream = File.OpenRead(filePath);
+
+        return GetLocalFileHash(stream);
+    }
+
+    public string GetLocalFileHash(Stream stream)
+    {
+        using SHA256 sha256 = SHA256.Create();
+
+        byte[] hash = sha256.ComputeHash(stream);
+        string version = WebEncoders.Base64UrlEncode(hash);
+
+        return version;
+    }
+}
---
+++ MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/Services/BrowserResourceHashing/WebResourceUrlsStorageService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.Collections.Concurrent;
+
+namespace MOSTComputers.UI.Web.Blazor.Services.BrowserResourceHashing;
+
+public class WebResourceUrlsStorageService
+{
+    public WebResourceUrlsStorageService(
+        WebResourceUrlsHashingService webResourceUrlsHashingService,
+        IWebHostEnvironment webHostEnvironment)
+    {
+        _webResourceUrlsHashingService = webResourceUrlsHashingService;
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public ConcurrentDictionary<string, Lazy<string>> _cachedUrls = new();
+
+    private readonly WebResourceUrlsHashingService _webResourceUrlsHashingService;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public string GetWebRootAssetUrlWithVersion(string url)
+    {
+        string version = GetWebRootAssetUrlVersion(url);
+
+        return $"{url}?v={version}";
+    }
+
+    public string GetWebRootAssetUrlVersion(string url)
+    {
+        string normalizedUrl = RemoveStartingSlashes(url);
+
+        Lazy<string> lazy = _cachedUrls.GetOrAdd(normalizedUrl, (url) =>
+        {
+            return new Lazy<string>(() =>
+            {
+                string fileUrl = Path.Combine(_webHostEnvironment.WebRootPath, url);
+
+                return _webResourceUrlsHashingService.GetLocalFileHash(fileUrl);
+            });
+        });
+
+        return lazy.Value;
+    }
+
+    public string GetBlazorAssetUrlWithVersion(string url)
+    {
+        string version = GetBlazorAssetUrlVersion(url);
+
+        return $"{url}?v={version}";
+    }
+
+    public string GetBlazorAssetUrlVersion(string url)
+    {
+        string normalizedUrl = RemoveStartingSlashes(url);
+
+        Lazy<string> lazy = _cachedUrls.GetOrAdd(normalizedUrl, (normalizedUrl) =>
+        {
+            return new Lazy<string>(() =>
+            {
+                IFileInfo fileInfo = _webHostEnvironment.WebRootFileProvider.GetFileInfo(normalizedUrl);
+
+                using Stream stream = fileInfo.CreateReadStream();
+
+                return _webResourceUrlsHashingService.GetLocalFileHash(stream);
+            });
+        });
+
+        return lazy.Value;
+    }
+
+    private static string RemoveStartingSlashes(string path)
+    {
+        path = path.Trim();
+
+        if (path.StartsWith("./"))
+        {
+            path = path[2..];
+        }
+
+        if (path.StartsWith('/'))
+        {
+            path = path[1..];
+        }
+
+        return path;
+    }
+}
