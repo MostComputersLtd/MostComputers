3e4d15787d3d45fdae610d10b5a85d75d46a210b
Most Computers <mostltddeveloper@gmail.com>
2026-05-07 18:25:21 +0300

    Added JS for pages that will be moving to SSR.

--- MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/wwwroot/js/Product/Promotions/Groups/PromotionGroupImagesViewer.js
+++ MOSTComputers.UI.Web.Blazor/MOSTComputers.UI.Web.Blazor/wwwroot/js/Product/Promotions/Groups/PromotionGroupImagesViewer.js
@@ -1,23 +1,87 @@
 ﻿const promotionGroupsListId = "promotionGroupsList";
+const promotionGroupItemName = "promotionGroupItem";
+
 const promotionImagesContainerId = "promotionImagesContainer";
-const promotionGroupItemName = "promotionGroupElement";
+
+const imageDataSourceAttribute = "data-src";
+const imagePromotionGroupIdAttribute = "data-promotion-group-id";
+
+const imageNotDisplayedClass = "not-displayed";
+const promotionGroupItemSelectedClass = "selected";
 
 async function selectGroupAndShowData(promotionGroupId, promotionGroupElementId, alwaysActivePromotionGroupId = null) {
-    var url = "/api/GetPromotionGroupImagesHtml?promotionGroupId=" + promotionGroupId;
 
-    if (alwaysActivePromotionGroupId != null) {
-        url += "&alwaysActivePromotionGroupId=" + alwaysActivePromotionGroupId;
+    const promotionImagesComponentContainer = document.getElementById(promotionImagesContainerId);
+
+    const promotionImagesContainer = promotionImagesComponentContainer.children[0];
+
+    const imageContainerElements = [...promotionImagesContainer.getElementsByTagName("div")];
+
+    for (let i = imageContainerElements.length - 1; i >= 0; i--) {
+
+        const imageContainerElement = imageContainerElements[i];
+
+        const imageElement = imageContainerElement.children[0];
+
+        const imagePromotionGroupId = imageElement.getAttribute(imagePromotionGroupIdAttribute);
+
+        const shouldImageBeVisible = parseInt(imagePromotionGroupId) === promotionGroupId;
+
+        if (!shouldImageBeVisible) continue;
+
+        const isHidden = imageContainerElement.classList.contains(imageNotDisplayedClass);
+
+        if (isHidden) {
+
+            const imageDataSource = imageElement.getAttribute(imageDataSourceAttribute);
+
+            if (imageDataSource != null) {
+
+                imageElement.src = imageDataSource;
+
+                imageElement.removeAttribute(imageDataSourceAttribute);
+            }
+
+            imageContainerElement.classList.remove(imageNotDisplayedClass);
+        }
+
+        if (imageContainerElement !== promotionImagesContainer.firstElementChild)
+        {
+            promotionImagesContainer.insertBefore(imageContainerElement, promotionImagesContainer.firstElementChild);
+        }
+
+        imageContainerElements.splice(i, 1);
     }
 
-    const newImagesHtmlResponse = await fetch(url,
-    {
-        method: "GET",
-        headers: {
-            'Accept': 'application/html'
-        }
-    });
+    for (const imageContainerElement of imageContainerElements) {
 
-    if (!newImagesHtmlResponse.ok) return;
+        const imageElement = imageContainerElement.children[0];
+        
+        const imagePromotionGroupId = imageElement.getAttribute(imagePromotionGroupIdAttribute);
+
+        const shouldImageBeVisible = parseInt(imagePromotionGroupId) === alwaysActivePromotionGroupId
+            && alwaysActivePromotionGroupId != null;
+
+        const isHidden = imageContainerElement.classList.contains(imageNotDisplayedClass);
+
+        if (shouldImageBeVisible) {
+            const imageDataSource = imageElement.getAttribute(imageDataSourceAttribute);
+
+            if (imageDataSource != null) {
+                imageElement.src = imageDataSource;
+
+                imageElement.removeAttribute(imageDataSourceAttribute);
+            }
+
+            imageContainerElement.classList.remove(imageNotDisplayedClass);
+        }
+        else if (!shouldImageBeVisible && !isHidden) {
+
+            imageContainerElement.classList.add(imageNotDisplayedClass);
+        }
+    }
+
+    promotionImagesContainer.scrollTop = 0;
 
     const promotionGroupElement = document.getElementById(promotionGroupElementId);
 
@@ -25,19 +89,15 @@ async function selectGroupAndShowData(promotionGroupId, promotionGroupElementId,
 
     const promotionGroupElements = promotionGroupsList.querySelectorAll("[name='" + promotionGroupItemName + "']");
 
-    for (const promotionGroupElement of promotionGroupElements) {
+    for (const element of promotionGroupElements) {
+
         if (element !== promotionGroupElement) {
-            element.classList.remove("active");
+
+            element.classList.remove(promotionGroupItemSelectedClass);
 
             continue;
         }
 
-        element.classList.add("active");
+        element.classList.add(promotionGroupItemSelectedClass);
     }
-
-    const newImagesHtml = await newImagesHtmlResponse.text();
-
-    const promotionImagesContainer = document.getElementById(promotionImagesContainerId); 
-
-    promotionImagesContainer.innerHTML = newImagesHtml;
 }
